916a783d644cdabfd89321b4651486ff516b830e
mkrecny <mkrecny@gmail.com>
2012-03-02 12:22:44 -0500

    Created the rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShelbyGtWeb::Application.config.secret_token = '024bc0743dbb01975233b779ea64aead1f54e0f983f18f26df1600659423ef725a6fb1ddab19a70cfbdfd32d1ca6ce14befc5809c7e52e9245a1b2b751835f32'
