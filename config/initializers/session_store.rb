3b9be27051af9678ff7ffaeee9740e6e96d39a38
mkrecny <mkrecny@gmail.com>
2012-03-16 15:03:57 -0400

    added secret token and cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ShelbyGtWeb::Application.config.session_store :cookie_store, key: '_shelby-gt-web_session'
+ShelbyGtWeb::Application.config.session_store :cookie_store, key: '_shelby_gt_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
