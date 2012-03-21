cd46c8de8fec8f89aa9a0510d6f25554ce6d72ed
Joshua Samberg <josh@shelby.tv>
2012-03-21 17:19:41 -0400

    Reformatted seo-friendly links

--- public/javascripts/app/utils/string.utils.js
+++ public/javascripts/app/utils/string.utils.js
@@ -2,6 +2,6 @@ libs.utils.String = {
   // sanitize a string for use as a url path segment
   toUrlSegment : function(string){
     // replace spaces and non-word characters with underscores
-    return string.replace(/\s+|\W+/g, '_');
+    return string.replace(/\s+|\W+/g, '-');
   },
  };
