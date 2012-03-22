41211821a5f504afbca6ccf65829f95c372e9ba5
mkrecny <mkrecny@gmail.com>
2012-03-22 17:48:29 -0400

    no URLs in conversations : )

--- public/javascripts/app/models/db/message.model.js
+++ public/javascripts/app/models/db/message.model.js
@@ -1,2 +1,10 @@
 libs.shelbyGT.MessageModel = Backbone.RelationalModel.extend({
+
+  getText : function(){
+    var regex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi);
+    var urls = regex.exec(this.get('text'));
+    if (!urls.length) return this.get('text');
+    return this.get('text').split(urls[0]).join('');
+  }
+
 });
