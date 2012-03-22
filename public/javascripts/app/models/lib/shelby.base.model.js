5d1f0574b2f893b844914b2b5694cad1d56bb844
mkrecny <mkrecny@gmail.com>
2012-03-22 10:22:38 -0400

    added user avatar to guide header

--- public/javascripts/app/models/lib/shelby.base.model.js
+++ public/javascripts/app/models/lib/shelby.base.model.js
@@ -4,10 +4,5 @@ libs.shelbyGT.ShelbyBaseModel = Backbone.RelationalModel.extend({
   // extract the JSON from the "result" property of a Shelby API response
   parse : function (response) {
     return response.result;
-  },
-
-  getFirstName : function(){
-    return this.get('name').split(' ')[0];
   }
-
 });
