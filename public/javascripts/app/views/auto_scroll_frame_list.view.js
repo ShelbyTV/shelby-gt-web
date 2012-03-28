3da17b1ecead7197e5936e06192232ce4dafa0e2
mkrecny <mkrecny@gmail.com>
2012-03-28 11:26:24 -0400

    active frame css class switch

--- public/javascripts/app/views/auto_scroll_frame_list.view.js
+++ public/javascripts/app/views/auto_scroll_frame_list.view.js
@@ -10,22 +10,30 @@
       ListView.prototype.initialize.call(this);
     },
 
-    _onNewActiveFrame : function(guideModel, frame){
-      // why is prevModel always null?
-      var oldModel = guideModel.previousAttributes().activeFrameModel;
-      /*var oldActiveFrameView = this.children.find(function(view){
-        return view.model == oldModel;
-      });
-      if (oldActiveFrameView){
-        oldActiveFrameView.$el.removeClass('active-frame');
-      }*/
-      var newActiveFrameView = this.children.find(function(view) {
-        return view.model == frame;
-      });
-      if (newActiveFrameView) {
-        newActiveFrameView.$el.addClass('active-frame');
-        this.parent.$el.scrollTo(newActiveFrameView.$el, {duration:200, axis:'y', offset:-9});
+    _findByViewModel : function(model){
+      return function(view){
+        return view.model == model;
       }
+    },
+
+    _getActiveFrameViews : function(guideModel, currentActiveFrameModel){
+      return {
+        current : this.children.find(this._findByViewModel(currentActiveFrameModel)),
+        old : this.children.find(this._findByViewModel(guideModel.previousAttributes().activeFrameModel))
+      };
+    },
+
+    _onNewActiveFrame : function(guideModel, currentActiveFrameModel){
+      var frameViews = this._getActiveFrameViews(guideModel, currentActiveFrameModel);
+      if (frameViews.current) {
+        this._switchActiveFrameViews(frameViews);
+      }
+    },
+
+    _switchActiveFrameViews : function(frameViews){
+      frameViews.old && frameViews.old.$el.removeClass('active-frame');
+      frameViews.current && frameViews.current.$el.addClass('active-frame');
+      this.parent.$el.scrollTo(frameViews.current.$el, {duration:200, axis:'y', offset:-9});
     },
 
     _cleanup : function(){
