3e26a56931003239568d4fe5c74b56e5eb6363f9
Henry Sztul <henry@sztul.com>
2012-04-10 16:34:27 -0400

    added event handlers

--- public/javascripts/app/views/user_preferences.view.js
+++ public/javascripts/app/views/user_preferences.view.js
@@ -1,11 +1,16 @@
 libs.shelbyGT.UserPreferencesView = Support.CompositeView.extend({
-
+  
+  events: {
+	  "click form.preferences .submit:not(.disabled)": 	"_submitPreferences"
+  },
+  
   template : function(obj){
     return JST['user-preferences'](obj);
   },
 
   render : function(){
     this.$el.html(this.template({user:this.model}));
-  }
-
+  },
+  
+  
 });
