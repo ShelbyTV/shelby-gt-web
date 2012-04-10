dd1ee2bbb3fcb6a79b4ac1ab17888663e35b1304
Henry Sztul <henry@sztul.com>
2012-04-10 18:19:53 -0400

    baby steps

--- public/javascripts/app/views/user_preferences.view.js
+++ public/javascripts/app/views/user_preferences.view.js
@@ -1,7 +1,9 @@
 libs.shelbyGT.UserPreferencesView = Support.CompositeView.extend({
   
+  preferences: this.model.get('preferences'),
+
   events: {
-	  "click form.preferences .submit:not(.disabled)": 	"_submitPreferences"
+	  "click .updateEmail": 	"_submitContactInfo"
   },
   
   template : function(obj){
@@ -11,6 +13,27 @@ libs.shelbyGT.UserPreferencesView = Support.CompositeView.extend({
   render : function(){
     this.$el.html(this.template({user:this.model}));
   },
+
+  _submitContactInfo: function(){
+    //this.model.save({primary_email:"henry@sztul.com"});
+	},
+	
+	_toggleWeeklyEmails: function(){
+		this.model.save({preferences: {weekly_emails: !this.preferences.weekly_emails}});
+	},
   
+	_toggleCommentEmails: function(){
+		this.model.save({preferences: {comment_emails: !this.preferences.comment_emails}});
+	},
+	
+	_toggleUpvoteEmails: function(){
+		this.model.save({preferences: {upvote_emails: !this.preferences.upvote_emails}});
+	},
+	
+	_toggleRerollEmails: function(){
+		this.model.save({preferences: {reroll_emails: !this.preferences.reroll_emails}});
+	}
+
+  // the future
   
 });
