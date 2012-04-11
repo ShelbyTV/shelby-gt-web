ec9bf71f67c403b2f91a7eb3281f447d39bf1c16
Henry Sztul <henry@sztul.com>
2012-04-11 10:44:54 -0400

    connecting other services and updating user info

--- public/javascripts/app/utils/oauth_popup.js
+++ public/javascripts/app/utils/oauth_popup.js
@@ -1,13 +1,17 @@
 /**
 	oAuth via popup window
 **/
-$("a.auth-popup").live('click', function(e){
+$("a.auth-popup, button.js-oauth-popup").live('click', function(e){
  	if( Browser.supportsAuthPopup() ){
 		var width = $(this).attr('popup-width');
 		var height = $(this).attr('popup-height');
 		var left = (screen.width/2)-(width/2);
 		var top = (screen.height/2)-(height/2);
-
+		
+		if (typeof $(this).attr('location') !== 'undefined'){
+			$(this).attr("href", $(this).attr('location'));
+		}
+		
 		window.open($(this).attr("href"), "authPopup", "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
 		e.stopPropagation(); return false;
 	}
--- public/javascripts/app/views/user_preferences.view.js
+++ public/javascripts/app/views/user_preferences.view.js
@@ -1,7 +1,9 @@
 libs.shelbyGT.UserPreferencesView = Support.CompositeView.extend({
 
   events: {
-	  "click .js-user-save": 	"_submitContactInfo"
+	  "click .js-user-save": 	"_submitContactInfo",
+		"click .js-user-cancel": "_cancel",
+		"change #you-preferences-email-weekly": "_toggleWeeklyEmails"
   },
   
   template : function(obj){
@@ -12,13 +14,32 @@ libs.shelbyGT.UserPreferencesView = Support.CompositeView.extend({
     this.$el.html(this.template({user:this.model}));
   },
 
+	_cancel: function(){
+		window.history.go(-2);
+	},
+
   _submitContactInfo: function(){
-	  console.log(this.$('.you-preferences-username').val());
-    //this.model.save({primary_email:"henry@sztul.com"});
+		var self = this;
+		var _email = this.$('#you-preferences-email').val();
+		var _username = this.$('#you-preferences-username').val();
+		var info = {primary_email: _email, nickname: _username};
+    this.model.save(info, {
+			success: function(){self._responseMessage("saved!");},
+			error: function(){self._responseMessage("error");}
+    });
+	},
+	
+	_responseMessage: function(msg){
+		var self = this;
+   	this.$('.js-response-message').show().text(msg);
+		setTimeout(function(){
+			self.$('.js-response-message').fadeOut('fast', function() { $(this).text(""); });
+		}, 3000);
 	},
 	
 	_toggleWeeklyEmails: function(){
-		this.model.save({preferences: {weekly_emails: !this.model.get('preferences').weekly_emails}});
+		var _val = this.$('#you-preferences-email-weekly').is(':checked') ? true : false;
+		this.model.save({preferences: {weekly_emails: _val}});
 	},
   
 	_toggleCommentEmails: function(){
