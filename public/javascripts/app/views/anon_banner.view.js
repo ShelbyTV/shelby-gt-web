bd8180db04e43da54d840d15c496c2a801a94351
Henry Sztul <henry@sztul.com>
2012-04-12 17:08:31 -0400

    made signin popup on l/o experience

--- public/javascripts/app/views/anon_banner.view.js
+++ public/javascripts/app/views/anon_banner.view.js
@@ -3,8 +3,6 @@
   libs.shelbyGT.AnonBannerView = Support.CompositeView.extend({
 
     events : {
-      "click .js-logged-out-twitter" : "_loginWithTwitter",
-      "click .js-logged-out-facebook" : "_loginWithFacebook"
     },
 
     //el : '',
@@ -21,18 +19,7 @@
     render : function(active){
       this.$el.html(this.template());
       $('#wrapper').append(this.$el);
-    },
-
-    _loginWithTwitter : function(){
-      console.log('logging in with twitter');
-      window.location = 'http://api.gt.shelby.tv/auth/twitter';
-    },
-
-    _loginWithFacebook : function(){
-      console.log('logging in with facebook');
-      window.location = 'http://api.gt.shelby.tv/auth/facebook';
     }
-
   });
 
 } ) ();
