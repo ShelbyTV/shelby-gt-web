cf6efef4cbc62a6c6937cea1d9f4d9ecdae3fcff
Joshua Samberg <josh@shelby.tv>
2012-04-17 18:35:08 -0400

    Fixed all edge cases for progressive loading

--- public/javascripts/app/views/paging_list.view.js
+++ public/javascripts/app/views/paging_list.view.js
@@ -35,12 +35,38 @@ libs.shelbyGT.PagingListView = libs.shelbyGT.ListView.extend({
   },
 
   _onItemsLoaded : function(rollModel, items){
-    this.$('.js-load-more').removeClass('js-loading').html('Load more');
+    this.$('.js-load-more').removeClass('js-loading');
+    this.$('.load-more-button').html('Load more');
     if (!this.options.infinite && items.length < this._loadCounts.requested) {
       // if the load returned less items than we requested, there are no more items to
-      // be loaded and we remove the DOM element that is clicked for more loading
-      this.$('.js-load-more').hide();
+      // be loaded and we hide the DOM element that is clicked for more loading
+      this._disableLoadMore();
     }
+  },
+
+  _onFetchSuccess : function(model, response){
+    if (!this.options.infinite && !this._doesResponseContainListCollection(response)) {
+      // special case - if a given load returns everything up to exactly the last item
+      // in the collection, the next load will not even contain the collection attribute - this
+      // is how the API responds when the skip and limit parameters restrict the result set
+      // to nothing
+
+      // since the relational:change handler (this._onItemsLoaded) will not be triggered in this case,
+      // manually hide the DOM element that is clicked for more loading
+      this._disableLoadMore();
+    }
+  },
+
+  _doesResponseContainListCollection : function(response) {
+    // HACK: subclasses must override to specify how to know when the response contains
+    // the collection that the list view is working for
+    // WE SHOULDN'T NEED TO DO THIS, THE API SHOULD RETURN AN EMPTY ARRAY FOR THE ATTRIBUTE
+    // WHEN THERE ARE NO RESULTS
+    console.log('Sorry, your PagingListView subclass must override _doesResponseContainListCollection');
+  },
+
+  _disableLoadMore : function(){
+    this.$('.js-load-more').hide();
   },
 
   addOne : function(item){
@@ -49,16 +75,19 @@ libs.shelbyGT.PagingListView = libs.shelbyGT.ListView.extend({
   },
 
   _loadMore : function(){
+    var self = this;
     var fetchData = {
       limit : this.options.limit,
       skip : this._loadCounts.total
     };
     _(fetchData).extend(this.options.fetchParams);
     this._loadCounts.requested = fetchData.limit;
-    this.$('.js-load-more').addClass('js-loading').html('Loading...');
+    this.$('.js-load-more').addClass('js-loading');
+    this.$('.load-more-button').html('Loading...');
     this.model.fetch({
       add : true,
-      data : fetchData
+      data : fetchData,
+      success: function(model, response){self._onFetchSuccess(model, response);}
     });
   }
 
