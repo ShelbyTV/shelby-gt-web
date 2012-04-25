eea7c8c64021ee51c7301c29fac4c881b7441316
Henry Sztul <henry@sztul.com>
2012-04-25 11:55:52 -0400

    checking if common cookie exists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # Simple helper to let us know if user is signed in:
   #  the _shelby_gt_common cookie is being set/cleared on the api server
   def user_signed_in?
-    if c = cookies[:_shelby_gt_common].split(',')[0]
+    if cookies[:_shelby_gt_common] and c = cookies[:_shelby_gt_common].split(',')[0]
       c.split("=")[1] == "nil" ? false : true
     else
       false
