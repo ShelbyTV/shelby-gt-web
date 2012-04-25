78ec2b6ccd28734b780dc97345b025e96d48ef09
Henry Sztul <henry@sztul.com>
2012-04-25 10:52:08 -0400

    really fixing user logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   # Simple helper to let us know if user is signed in:
   #  the _shelby_gt_common cookie is being set/cleared on the api server
   def user_signed_in?
-    if cookie = cookies[:_shelby_gt_common]
-      cookie.split("=")[1] == "nil" ? false : true
+    if c = cookies[:_shelby_gt_common].split(',')[0]
+      c.split("=")[1] == "nil" ? false : true
     else
       false
     end
