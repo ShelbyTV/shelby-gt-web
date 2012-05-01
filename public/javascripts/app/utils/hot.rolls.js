3c976dc0354c8c29769d41ddd8b16872b0f4a69d
mkrecny <mkrecny@gmail.com>
2012-05-01 17:21:31 -0400

    keyword public java fix

--- public/javascripts/app/utils/hot.rolls.js
+++ public/javascripts/app/utils/hot.rolls.js
@@ -4,7 +4,7 @@
       collaborative: false,
       creator_id: "4d877a47f6db2414e2000001",
       id: "4f901bd8b415cc466a0008ec",
-      //public: true,
+      'public': true,
       thumbnail_url: "http://a0.twimg.com/profile_images/1285777010/Screen_shot_2011-03-24_at_4.03.26_PM_normal.png",
       title: "onshelby"
     },
@@ -12,7 +12,7 @@
       collaborative: false,
       creator_id: "4f900cf4b415cc466a0005b9",
       id: "4f900cf5b415cc466a0005bb",
-      //public: true,
+      'public': true,
       thumbnail_url: "http://b.vimeocdn.com/ts/265/710/265710329_1280.jpg",
       title: "Vimeo"
     },
@@ -21,7 +21,7 @@
       creator_id: "4f7c9f89ad9f11318c000223",
       id: "4fa03429b415cc18bf0007b2",
       origin_network: null,
-      //public: true,
+      'public': true,
       thumbnail_url: null,
       title: "Hipster Sounds"
     }
