809fc0240fba9cc903e3619ab23fb419ddf2ccf7
Michael Matyus <michael@maty.us>
2012-05-02 14:14:28 -0400

    attempt at using scrollTo for team page

--- public/javascripts/app/views/team.view.js
+++ public/javascripts/app/views/team.view.js
@@ -1,4 +1,8 @@
 libs.shelbyGT.TeamView = Support.CompositeView.extend({
+
+	events : {
+		"click .js-about-team-employee" : "_scrollToElement"
+	},
   
   template : function(obj){
     return JST['team'](obj);
@@ -6,6 +10,11 @@ libs.shelbyGT.TeamView = Support.CompositeView.extend({
 
   render : function(){
     this.$el.html(this.template({user:this.model}));
+  },
+
+  _scrollToElement : function(e){
+  	$('#js-guide-wrapper').scrollTo($(e.currentTarget.hash.replace('#','.')),{duration: 200, axis: 'y', offset:{top: -10}});
+
   }
 
 });
