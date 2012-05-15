2fd52dc919c1db64ff66b7c971a8fc767594b2b2
Henry Sztul <henry@sztul.com>
2012-05-15 10:06:41 -0400

    updated share button and secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ShelbyGtWeb::Application.config.secret_token = 'bf4cd3afd9fe651da3536bead5e415ca5fff75142c716df3deb6fd537be93eea3e8deb5cf25bd4c36267714bc65a1911466e8afda86b09220636b074355eb68c'
+ShelbyGtWeb::Application.config.secret_token = 'bf4cd3afd9fe651da3536bead5e415ca5fff75142c716df3deb6fd537be93eea3e8deb5cf25bd4c36267714bc65a1911466e8afda86b09220636b074355eb68d'
