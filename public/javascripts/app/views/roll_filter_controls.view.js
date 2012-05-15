20309575592c8066be62460089cee979c8a699bb
Henry Sztul <henry@sztul.com>
2012-05-15 15:07:37 -0400

    if you are a roll creator you can edit the title of roll

--- public/javascripts/app/views/roll_filter_controls.view.js
+++ public/javascripts/app/views/roll_filter_controls.view.js
@@ -3,7 +3,10 @@ libs.shelbyGT.RollFilterControlsView = Support.CompositeView.extend({
   events : {
     "click #js-rolls-back" : "_goBackToRollsList",
     "click #js-roll-back" : "_goToPreviousRoll",
-    "click #js-roll-next" : "_goToNextRoll"
+    "click #js-roll-next" : "_goToNextRoll",
+		"click .js-roll-title-text" : "_showRollNameEditInput",
+		"focusout .js-roll-name-change" : "_closeInputArea",
+		"keypress .js-roll-name-change" : "_onEnterInInputArea"
   },
 
   tagName : 'div',
@@ -38,6 +41,30 @@ libs.shelbyGT.RollFilterControlsView = Support.CompositeView.extend({
   _goToNextRoll : function(){
     var nextRoll = shelby.models.user.getNextRoll(this.model);
     shelby.router.navigateToRoll(nextRoll, {trigger:true,replace:true});
-  }
+  },
+
+	_showRollNameEditInput : function(){
+		if (this.model.get('creator_id') == shelby.models.user.id){
+			var rollName = this.model.get('title');
+			this.$('.js-roll-title-text').replaceWith('<input class="js-roll-name-change" value="'+rollName+'"/ size="15">');
+			this.$('input.js-roll-name-change').focus();
+		}
+	},
+	
+	_onEnterInInputArea : function(){
+		if (event.keyCode==13){
+			return this._editRollName();
+		}
+	},
+	
+	_editRollName : function(){
+		var self = this;
+		var _newTitle = this.$('.js-roll-name-change').val();
+    this.model.save({title: _newTitle});
+	},
+	
+	_closeInputArea : function(){
+		this.$('input.js-roll-name-change').replaceWith('<span class="js-roll-title-text">'+this.model.get('title')+'</span>');
+	}
 
 });
