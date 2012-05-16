73daef30e756f44868a82aeeed32c2ed8ac17a95
mkrecny <mkrecny@gmail.com>
2012-05-16 17:55:31 -0400

    app_progress has persistence

--- public/javascripts/app/views/frame.view.js
+++ public/javascripts/app/views/frame.view.js
@@ -272,7 +272,7 @@ libs.shelbyGT.FrameView = ListItemView.extend({
   },
 
   _onDoFrameRollingChange : function(frameStateModel, doFrameRolling) {
-    shelby.models.userProgress.set('framesRolled', shelby.models.userProgress.get('framesRolled') + (doFrameRolling ? 1 : -1));
+    shelby.models.user.get('app_progress').set('framesRolled', shelby.models.userProgress.get('framesRolled') + (doFrameRolling ? 1 : -1));
     if (doFrameRolling) {
 			// hard core rolling action
       this._roll();
