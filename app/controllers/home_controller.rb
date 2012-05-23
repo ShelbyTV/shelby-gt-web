8250c6868f9eb33ed9b90e843b6326acdcb08789
Henry Sztul <henry@sztul.com>
2012-05-23 10:22:02 -0400

    added tedtalks.tv

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -40,6 +40,7 @@ class HomeController < ApplicationController
       when "laughingsquid.tv" then "4fa28d309a725b77f700070f"
       when "hipstersounds.tv" then "4fa03429b415cc18bf0007b2"
       when "reecepacheco.tv" then "4f900d56b415cc6614056681"
+      when "tedtalks.tv" then "4fbaa51d1c1cf44b9d002f58"
       when "localhost.hipstersounds.tv" then "4fa03429b415cc18bf0007b2"
       when "localhost.danspinosa.tv" then "4f8f7ef2b415cc4762000002"
       when "localhost.henrysztul.tv" then "4f8f7ef6b415cc476200004a"
