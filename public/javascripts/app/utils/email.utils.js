9b99fbfb540890b4144a6a5acaad2ce332fa55be
mkrecny <mkrecny@gmail.com>
2012-06-07 12:02:52 -0400

    add custom error handling

--- public/javascripts/app/utils/email.utils.js
+++ public/javascripts/app/utils/email.utils.js
@@ -1,22 +1,22 @@
 (function(){
   libs.utils.email = {
 
-    apiRoot : window.location.host.indexOf('localhost')===-1 ? 'http://50.57.150.47:4000' : 'http://localhost:4000',
-    
+    apiRoot : function(){
+      return window.location.host.indexOf('localhost')!==-1 ? 'http://localhost:4000' : 'http://50.57.150.47:4000';
+    }, 
+
     publishCurrentFrame : function(){
       libs.utils.email.sendRollNotification(shelby.models.guide.get('activeFrameModel'));
     },
     
     // this fn needs to be called and passed the 'just rolled' frame -- how do we get a ref to that?
     publishFrameAddition : function(frame){
-      return;
       var self = this;
       // don't publish if we're in localhost
-      if (window.location.host.indexOf('localhost')!==-1) return;
-      console.log('here');
+      //if (window.location.host.indexOf('localhost')!==-1) return;
       jQuery.ajax({
         type : 'POST',
-        url : self.apiRoot+'/roll/'+frame.get('roll_id')+'/publish',
+        url : self.apiRoot()+'/roll/'+frame.get('roll_id')+'/publish',
         data : {
           title : frame.get('video').get('title'),
           roll_title : frame.get('roll').get('title'),
@@ -25,7 +25,7 @@
           frame_id : frame.id
         },
         error : function(){
-          console.log('oh no error', arguments);
+          console.log("couldn't contact local pubsub API .. not a big deal, carry on");
         },
         success : function(){
           //do nothing
@@ -33,4 +33,5 @@
       });
     }
   };
+
 })();
