a994d123111c1057488276d2e705d2cae17a6cc6
Michael Matyus <michael@maty.us>
2012-06-08 13:53:19 -0400

    working to make opera svg friendly, deleting the png fallback for opera

--- public/javascripts/common/modernizr.init.js
+++ public/javascripts/common/modernizr.init.js
@@ -13,11 +13,4 @@ $(function(){
       guide.addEventListener('DOMSubtreeModified', refresh, false);
     }
   }
-  //opera returns the "opera" object,
-  //exploiting this because Opera's SVG background support is shaky,
-  //so, load the png fallback css and enable the proper class.
-  if(window.opera){
-    $('html').removeClass('svg').addClass('no-svg')
-             .children('head').append('<link href="/stylesheets/png.css" media="screen" rel="stylesheet" type="text/css"/>');
-  }
 });
