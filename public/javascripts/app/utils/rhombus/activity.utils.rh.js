c011cb4508f802c74e531b15bce6a0134ca25d8a
mkrecny <mkrecny@gmail.com>
2012-06-20 12:01:50 -0400

    attempting @spinosa s suggestion re leave

--- public/javascripts/app/utils/rhombus/activity.utils.rh.js
+++ public/javascripts/app/utils/rhombus/activity.utils.rh.js
@@ -1,6 +1,6 @@
 (function(){
 
-  var _interval = 3*60*1000; 
+  var _interval = 3*60*1000; //3 mins
   setTimeout(function(){
     libs.utils.rhombus.sadd('active_web', shelby.models.user.id);
   }, _interval);
