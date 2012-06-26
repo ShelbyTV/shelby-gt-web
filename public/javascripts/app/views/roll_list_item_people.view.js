681696a22f67edd5e451716f680bd528061bee62
Michael Matyus <michael@maty.us>
2012-06-26 14:14:05 -0400

    adding view for people tab in the rolls list

---
+++ public/javascripts/app/views/roll_list_item_people.view.js
@@ -0,0 +1,41 @@
+libs.shelbyGT.RollItemViewPeople = libs.shelbyGT.ActiveHighlightListItemView.extend({
+
+  options : _.extend({}, libs.shelbyGT.ActiveHighlightListItemView.prototype.options, {
+      activationStateProperty : 'activeFrameModel'
+  }),
+
+  events : {
+    "click .js-roll-item-button"          : "goToRoll",
+    "click .roll-item-stats"              : "goToRoll",
+    "click .roll-item-contents-thumbnail" : "goToRoll"
+  },
+
+  tagName : 'li',
+
+  className : 'roll-item clearfix',
+
+  template : function(obj){
+    return JST['roll-item-people'](obj);
+  },
+
+  render : function(){
+    this.$el.html(this.template({roll : this.model}));
+    libs.shelbyGT.ActiveHighlightListItemView.prototype.render.call(this);
+  },
+
+  goToRoll : function(){
+    shelby.router.navigateToRoll(this.model, {trigger:true});
+  },
+
+  // override ActiveHighlightListItemView abstract method
+  doActivateThisItem : function(guideModel){
+    var activeFrameModel = guideModel.get('activeFrameModel');
+    if (activeFrameModel) {
+      var roll = activeFrameModel.get('roll');
+      if (roll && this.model.id == roll.id) {
+        return true;
+      }
+    }
+    return false;
+  }
+});
