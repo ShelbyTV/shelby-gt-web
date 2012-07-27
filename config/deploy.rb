a6ad8565aec31d1a1489487c6b3a3747877dabe2
Joshua Samberg <josh@shelby.tv>
2012-07-27 14:00:05 -0400

    Completely remove our old custom bundler tasks, which are now being done by bundler/capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,35 +31,6 @@ namespace :deploy do
 end
 
 #############################################################
-#	Bundler
-#############################################################
-
-# namespace :bundler do
-#   task :create_symlink, :roles => :app do
-#     shared_dir = File.join(shared_path, 'bundle')
-#     release_dir = File.join(current_release, '.bundle')
-#     run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
-#   end
-
-#   task :bundle_new_release, :roles => :app do
-#     bundler.create_symlink
-#     run "cd #{release_path} && bundle install --without test"
-#   end
-
-#   task :lock, :roles => :app do
-#     run "cd #{current_release} && bundle lock;"
-#   end
-
-#   task :unlock, :roles => :app do
-#     run "cd #{current_release} && bundle unlock;"
-#   end
-# end
-
-# after "deploy:update_code" do
-#   bundler.bundle_new_release
-# end
-
-#############################################################
 #	Multistage Deploy via capistrano-ext
 #############################################################
 
