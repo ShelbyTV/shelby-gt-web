2b0c771189a9e66b3e452fb1415e08bff02d2f3a
Michael Matyus <michael@maty.us>
2012-07-27 11:30:52 -0400

    merge new master, fix conflicts

---
+++ public/javascripts/compiled/app.config.js
@@ -0,0 +1,8 @@
+shelby = {
+  config : {
+    apiBase : 'http://api.shelby.tv',
+    apiRoot : 'http://api.shelby.tv/v1',
+    appUrl : 'http://localhost.shelby.tv:3000',
+    environment : 'development'
+  }
+};
