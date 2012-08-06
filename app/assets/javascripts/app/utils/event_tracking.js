883009f517f3734455d756cb5491999aee8674b0
Henry Sztul <henry@sztul.com>
2012-08-06 14:49:04 -0400

    changes to allow conversion tracking to current gate page

--- app/assets/javascripts/app/utils/event_tracking.js
+++ app/assets/javascripts/app/utils/event_tracking.js
@@ -5,7 +5,7 @@
 // add class 'js-track-event' to element with data attributes for a category, action and label
 //---------------------------------------------------------
 $(document).ready(function(){
-	$('.js-track-event').live('click',function(e){
+	$('.js-track-event').on('click',function(e){
 		try{
 			_gaq.push(['_trackEvent', $(e.currentTarget).data("ga_category"), $(e.currentTarget).data("ga_action"), $(e.currentTarget).data("ga_label")]);
 		}
--- app/assets/javascripts/common/event_tracking.js
+++ app/assets/javascripts/common/event_tracking.js
@@ -5,33 +5,42 @@
 // add class 'js-track-event' to element with data attributes for a category, action and label
 //---------------------------------------------------------
 $(document).ready(function(){
-	$('.js-track-event').live('click',function(e){
-	  
+	$('.js-track-event').on('click', function(e){
+    
 	  var action = $(e.currentTarget).data("ga_action");
 	  var category = $(e.currentTarget).data("ga_category");
 	  var label = $(e.currentTarget).data("ga_label");
-	  
-	  if (typeof $.getUrlParam('awesm') != 'undefined')
+	  var kiss_action = action;
 	  
 	  switch (action){
       case 'Submitted email':
-        _kmq.push(['record', action]);
+        kiss_action = 'Submitted email on landing page';
         break;
-      case 'beta_user_login':
-        _kmq.push(['record', 'click beta user login']);
+      case 'Beta User Login':
+        kiss_action = 'Clicked beta user login on landing page';
         break;
-      case 'login w twitter':
-        _kmq.push(['record', 'twitter login on landing page']);
+      case 'Login with Twitter':
+        kiss_action = 'Login with Twitter on landing page';
         break;
-      case 'login w facebook':
-        _kmq.push(['record', 'facebook login on landing page']);
+      case 'Login with Facebook':
+        kiss_action = 'Login with Facebook on landing page';
         break;
-      case 'identify':
-        _kmq.push(['identify', options.nickname]);
+      case 'Not a Beta User':
+        kiss_action = "Click not a beta user on landing page";
         break;
     };
 	  
-		try{
+	  // Check if visitor is coming via a shl.by short link
+	  //  NOTE: this is know because of the url param: ?awesm=shl.by_jk
+	  var awesmLink = $.getUrlParam('awesm');
+	  if (typeof awesmLink != 'undefined') {
+	    category = category + ' via Short Link';
+	    kiss_action = action + ' via Short Link';
+	    label = awesmLink;
+	  }
+	  
+ 		try{
+		  _kmq.push(['record', kiss_action]);
 			_gaq.push(['_trackEvent', category, action, label]);
 		}
 		catch(e){};
