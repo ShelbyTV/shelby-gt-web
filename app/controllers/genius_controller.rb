92e843bfb0b3047cb7dda6621ffb53b4101cf104
Henry Sztul <henry@sztul.com>
2012-08-09 16:22:28 -0400

    functionality there for a prototype

--- app/controllers/genius_controller.rb
+++ app/controllers/genius_controller.rb
@@ -3,25 +3,4 @@ class GeniusController < ApplicationController
   # Nothing here for now, but likely will need some logic soon, so still setting
   # up as controller + views instead of true non-Rails static content
   
-  def mobile_search
-    # render just search box
-    @query_param = params[:query]
-  end
-  
-  def mobile_results
-    if params[:term]
-      # do yt search and show results page
-      @term = params[:term]#.gsub!(/\W/, '-')
-      client = YouTubeIt::Client.new
-      search_result = client.videos_by(:query => @term, :order_by => "relevance", :per_page => 50)
-      @urls = []
-      search_result.videos.each { |v| @urls << v.player_url }
-      @urls = [@urls.join(',')].to_json
-      # post to shelby to get genius
-      if @roll = Shelby::API.post_to_genius(@term, @urls) and @frames = Shelby::API.get_frames_in_roll(@roll["id"])
-        
-      end
-    end
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 ShelbyGtWeb::Application.routes.draw do
 
   get '/genius' => 'genius#index'
-  get '/m(/*query)' => 'genius#mobile_search'
-  post '/m/results' => 'genius#mobile_results'
+  get '/m(/*query)' => 'mobile#mobile_search'
+  post '/m/results' => 'mobile#mobile_results'
 
   get '/video/:provider_name/:provider_id(/*title)' => "seovideo#show"
   
