85a2d642c4d76ba26569eb83815d19073e20bfc3
Daniel Jeng <dj98@cornell.edu>
2012-08-10 15:33:33 -0400

    added radar.js

--- app/assets/javascripts/radar.js
+++ app/assets/javascripts/radar.js
@@ -1,10 +1,1 @@
-//= require ../templates/bookmarklet-buttons.jst.ejs
-//= require ../templates/bookmarklet-context-content.jst.ejs
-//= require ../templates/bookmarklet-context.jst.ejs
-//= require ../templates/bookmarklet-login.jst.ejs
-//= require ../templates/bookmarklet-video-tab.jst.ejs
-//= require ../templates/rolling-form.jst.ejs
-//= require ../templates/frame-rolling.jst.ejs
-//= require ../templates/frame-conversation.jst.ejs
-//= require ../templates/wide-thumbnail-overlay.jst.ejs
-//= require ../templates/message.jst
+//= require_tree ../templates
