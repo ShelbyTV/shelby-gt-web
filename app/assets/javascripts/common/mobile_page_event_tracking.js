d9836cd8d591e51e661da1e8e9140a76754c4950
Henry Sztul <henry@sztul.com>
2012-08-17 12:24:32 -0400

    added some event tracking for mobile

---
+++ app/assets/javascripts/common/mobile_page_event_tracking.js
@@ -0,0 +1,39 @@
+//---------------------------------------------------------
+// Google Analytics and KISSMetrics Event Tracking
+// bind click events to buttons and a tags 
+// To Use:
+// add class 'js-track-event' to element with data attributes for a category, action and label
+//---------------------------------------------------------
+$(document).ready(function(){
+	$('.js-track-event').on('click', function(e){
+    
+	  var action = $(e.currentTarget).data("ga_action");
+	  var category = $(e.currentTarget).data("ga_category");
+	  var label = $(e.currentTarget).data("ga_label");
+	  
+	  switch (action){
+      case 'Clicked previous search':
+        action = 'Clicked previous search on mobile';
+        break;
+      case 'Removed previous search':
+        action = 'Removed previous search on mobile';
+        break;
+      case 'Clicked promoted search':
+        action = 'Clicked promoted search on mobile';
+        break;
+      case 'Clicked back to search':
+        action = 'Clicked back to search on mobile';
+        break;
+      case 'Clicked share button':
+        action = 'Clicked share button on mobile';
+        break;
+        
+    };
+	  
+ 		try{
+		  _kmq.push(['record', action]);
+			_gaq.push(['_trackEvent', category, action, label]);
+		}
+		catch(e){};
+	});
+});
