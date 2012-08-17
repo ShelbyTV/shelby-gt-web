746233b3b9d4237c6cd1645d6ca8720216cc4c5f
Henry Sztul <henry@sztul.com>
2012-08-17 13:11:00 -0400

    removed share button. updated js layout for mobile

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,4 +1,10 @@
-//= require_tree ./common
+//= require_tree ./mobile
+//= require ./common/modernizr-2.5.3.min.js
+//= require ./common/modernizr.init.js
+//= require ./common/ytquery.js
+//= require ./common/spin.js
+//= require ./common/iscroll.js
+
 
 //jQuery url params plugin
 //= require ./jquery-plugins/jquery.urlparams.js
