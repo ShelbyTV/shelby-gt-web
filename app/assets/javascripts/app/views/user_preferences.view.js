a3cdeddf97a143e7e2affd9323de69c10043170f
Joshua Samberg <josh@shelby.tv>
2012-09-05 22:00:12 +0200

    Preferences: remove unused notifications,fix remaining notifications;fixes #410

--- app/assets/javascripts/app/views/user_preferences.view.js
+++ app/assets/javascripts/app/views/user_preferences.view.js
@@ -5,9 +5,7 @@ libs.shelbyGT.UserPreferencesView = Support.CompositeView.extend({
     "click .js-user-password-save:not(.js-busy)": "_submitPassword",
     "click .js-user-cancel": "_cancel",
     "click #show-change-password": "_showChangePassword",
-    "change #you-preferences-email-weekly": "_toggleWeeklyEmails",
-    "change #you-preferences-email-watches": "_toggleWatchEmails",
-    "change #you-preferences-email-upvotes": "_toggleUpvoteEmails",
+    "change #you-preferences-email-updates": "_toggleEmailUpdates",
     "change #you-preferences-email-comments": "_toggleCommentEmails",
     "change #you-preferences-email-rerolls": "_toggleRerollEmails",
     "change #you-preferences-email-joinrolls": "_toggleJoinrollEmails",
@@ -119,27 +117,15 @@ libs.shelbyGT.UserPreferencesView = Support.CompositeView.extend({
     }, 3000);
   },
 
-  _toggleWeeklyEmails: function(){
+  _toggleEmailUpdates: function(){
     var _prefs = this.model.get('preferences');
-    _prefs.email_updates = this.$('#you-preferences-email-weekly').is(':checked') ? true : false;
+    _prefs.email_updates = this.$('#you-preferences-email-updates').is(':checked') ? true : false;
     this.model.save({preferences: _prefs});
   },
 
   _toggleCommentEmails: function(){
     var _prefs = this.model.get('preferences');
     _prefs.comment_notifications = this.$('#you-preferences-email-comments').is(':checked') ? true : false;
-    this.model.save({preferences: _prefs});
-  },
-
-  _toggleUpvoteEmails: function(){
-    var _prefs = this.model.get('preferences');
-    _prefs.upvote_notifications = this.$('#you-preferences-email-upvotes').is(':checked') ? true : false;
-    this.model.save({preferences: _prefs});
-  },
-
-  _toggleWatchEmails: function(){
-    var _prefs = this.model.get('preferences');
-    _prefs.watched_notifications = this.$('#you-preferences-email-watches').is(':checked') ? true : false;
     this.model.save({preferences: _prefs});
   },
 
