879b86820b1f819b94fe985101a6880a43695380
Dan Spinosa <spinosa@gmail.com>
2012-09-11 10:44:21 -0400

    style and functionality (commenting, user avatar, backbone compatibility) fixes for extension code. regression tested on main site.  Fixes #415

--- app/assets/javascripts/app/utils/view_helpers/user_view_helper.js
+++ app/assets/javascripts/app/utils/view_helpers/user_view_helper.js
@@ -16,7 +16,7 @@ libs.shelbyGT.viewHelpers.user = {
       return libs.shelbyGT.viewHelpers.user.undefinedAvatarUrl;
     } else if( user.get('has_shelby_avatar') ){
       //using .get('id') for extension compatibility
-      return shelby.config.avatarUrlRoot+'/'+avatarSize+'/'+user.get('id')+'?'+Date.parse(user.get('avatar_updated_at'));
+      return shelby.config.avatarUrlRoot+'/'+avatarSize+'/'+user.get('id')+'?'+(new Date(user.get('avatar_updated_at')).getTime());
     } else {
       return user.get('user_image_original') || user.get('user_image') || libs.shelbyGT.viewHelpers.user.undefinedAvatarUrl;
     }
