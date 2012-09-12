0d9ae78f783c19ccc4a20c97685bf2f00af84fbc
Joshua Samberg <josh@shelby.tv>
2012-09-12 14:45:28 +0200

    Split 'Me' section into two lists: rolls I created and rolls I didn't create;fixes #394

--- app/assets/javascripts/app/views/me_list.view.js
+++ app/assets/javascripts/app/views/me_list.view.js
@@ -6,10 +6,26 @@ libs.shelbyGT.MeListView = Support.CompositeView.extend({
 
   initialize : function() {
     this.render();
+    this.model.bind('change:initialized', this._onRollFollowingsInitialized, this);
+  },
+
+  _cleanup : function() {
+    this.model.unbind('change:initialized', this._onRollFollowingsInitialized, this);
   },
 
   render : function(){
     this.$el.html(this.template());
+    if (this.model.get('initialized')) {
+      this._showHeaders();
+    }
+    this.appendChildInto(new libs.shelbyGT.RollListView({
+      binarySearchOffset : shelby.config.db.rollFollowings.numSpecialRolls,
+      model : this.model,
+      doSmartRefresh : !this.model.get('rolls').isEmpty(),
+      doStaticRender : true,
+      listItemView : 'RollItemRollView',
+      rollListFilterType : libs.shelbyGT.RollListFilterType.me
+    }), '.js-me-rolls-list');
     this.appendChildInto(new libs.shelbyGT.RollListView({
       binarySearchOffset : 0,
       model : this.model,
@@ -18,6 +34,17 @@ libs.shelbyGT.MeListView = Support.CompositeView.extend({
       listItemView : 'RollItemRollView',
       rollListFilterType : libs.shelbyGT.RollListFilterType.following
     }), '.js-following-rolls-list');
+  },
+
+  _onRollFollowingsInitialized : function(rollFollowingsModel, initialized) {
+    if (initialized) {
+      this._showHeaders();
+    }
+  },
+
+  _showHeaders : function() {
+    this.$('.js-me-header').show();
+    this.$('.js-following-header').show();
   }
 
 });
