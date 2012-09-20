595ae423e5a877a6a0634766493b192f75dbea35
Joshua Samberg <josh@shelby.tv>
2012-09-20 17:34:14 -0400

    Add a message to dashboard when it fetch completes and it is empty - still needs to be styled

--- app/assets/javascripts/app/index.js
+++ app/assets/javascripts/app/index.js
@@ -19,6 +19,7 @@ without triggering default app setup actions such as routing, user fetch, etc. *
 //= require ./views/smart_refresh_list.view.js
 //= require ./views/paging_list.view.js
 //= require ./views/frame_group_play_paging_list.view.js
+//= require ./views/dashboard_empty_indicator.view.js
 //= require ./views/dashboard.view.js
 //= require ./views/guide_overlay.view.js
 //= require ./views/me_list.view.js
