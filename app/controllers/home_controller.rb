ee50f1e178ce96ca00d37719b84b85e2db849895
Henry Sztul <henry@sztul.com>
2012-09-25 14:34:26 -0400

    making personalization clickable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -68,7 +68,9 @@ class HomeController < ApplicationController
   # main search/home page for fb genius app
   #
   def facebook_genius_index
-    if params[:signed_request]
+    if frame = params[:frame] and roll = params[:roll]
+      return redirect_to fb_genius_frame_path(:roll_id => roll, :frame_id => frame)
+    elsif params[:signed_request]
       koala = Koala::Facebook::OAuth.new(Settings::Facebook.genius_app_id, Settings::Facebook.genius_app_secret)
       @un_signed_request = koala.parse_signed_request(params[:signed_request])
     end
