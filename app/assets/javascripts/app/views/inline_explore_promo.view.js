d5a02fda3898bc2d01b0245f99c5ac88de638780
Joshua Samberg <josh@shelby.tv>
2012-10-11 14:41:58 -0400

    Link to full app when clicking promos within iso roll;fixes #682

--- app/assets/javascripts/app/views/inline_explore_promo.view.js
+++ app/assets/javascripts/app/views/inline_explore_promo.view.js
@@ -16,7 +16,11 @@ libs.shelbyGT.InlineExplorePromoView = Support.CompositeView.extend({
   },
 
   _gotoExplore : function(){
-    shelby.router.navigate('explore', {trigger:true});
+    if (shelby.models.guide.get('displayIsolatedRoll')) {
+      window.top.location.href = shelby.config.appUrl + '/explore';
+    } else {
+      shelby.router.navigate('explore', {trigger:true});
+    }
     shelby.track('Click explore promo');
   }
 
