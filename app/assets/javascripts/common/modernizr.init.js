1f003893648dd4f6b3c44147a806668e3112bd54
Michael Matyus <michael@maty.us>
2012-10-12 17:58:31 -0400

    shit

--- app/assets/javascripts/common/modernizr.init.js
+++ app/assets/javascripts/common/modernizr.init.js
@@ -1,14 +1,3 @@
 $(function(){
-  if(Modernizr.touch){
     //if touch-screen enabled, use iscroll
-    console.log('iScroll enabled');
-
-    var myScroll;
-
-    function loaded() {
-      myScroll = new iScroll('js-guide-body');
-    }
-    document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
-    document.addEventListener('DOMContentLoaded', function () { setTimeout(loaded, 200); }, false);
-  }
 });
