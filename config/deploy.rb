6b584b61c6b5b8a03a64d66472b49dd31984d5ef
Joshua Samberg <josh@shelby.tv>
2012-10-15 16:07:38 -0400

    Attempted to add unicorn config

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,36 +17,6 @@ set :deploy_via, :remote_cache
 # Use developer's local ssh keys when git clone/updating on the remote server
 ssh_options[:forward_agent] = true
 
-
-#############################################################
-#	Bundler
-#############################################################
-
-namespace :bundler do
-  task :symlink, :roles => :app do
-    shared_dir = File.join(shared_path, 'bundle')
-    release_dir = File.join(current_release, '.bundle')
-    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
-  end
-
-  task :bundle_new_release, :roles => :app do
-    bundler.symlink
-    run "cd #{release_path} && bundle install --without test"
-  end
-
-  task :lock, :roles => :app do
-    run "cd #{current_release} && bundle lock;"
-  end
-
-  task :unlock, :roles => :app do
-    run "cd #{current_release} && bundle unlock;"
-  end
-end
-
-after "deploy:update_code" do
- bundler.bundle_new_release
-end
-
 #############################################################
 #	Multistage Deploy via capistrano-ext
 #############################################################
