4cc01bb779a4f16e38a6a1a6294afe1eb1115cc0
Henry Sztul <henry@sztul.com>
2012-10-17 16:29:56 -0400

    added .tv to home controller for @ckurdziel

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -95,6 +95,7 @@ class HomeController < ApplicationController
           when "tedtalks.tv" then "4fbaa51d1c1cf44b9d002f58"
           when "trololo.shelby.tv" then "4fccc6e4b415cc7f2100092d"
           when "wallstreetjournal.tv" then "4fa8542c88ba6b669b000bcd"
+          when "yvynyl.tv" then "4fa2908088ba6b61770010af"
           when "localhost.hipstersounds.tv" then "4fa03429b415cc18bf0007b2"
           when "localhost.danspinosa.tv" then "4f8f7ef2b415cc4762000002"
           when "localhost.henrysztul.tv" then "4f8f7ef6b415cc476200004a"
