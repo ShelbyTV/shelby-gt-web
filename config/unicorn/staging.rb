8f0aa86e954ff47d60b0c19cc53df5ad36515f38
Joshua Samberg <josh@shelby.tv>
2012-10-18 13:18:44 -0400

    Get unicorn to run under rails staging env in staging

---
+++ config/unicorn/staging.rb
@@ -0,0 +1,42 @@
+# Set your full path to application.
+app_path = "/home/gt/web/current"
+
+# Set unicorn options
+worker_processes 4
+preload_app true
+timeout 30
+listen "/tmp/shelby-web.socket", :backlog => 64
+#listen 8080, :tcp_nopush => true
+
+# Spawn unicorn master worker for user apps (group: apps)
+user 'gt'
+
+# Fill path to your app
+working_directory app_path
+
+# Should be 'staging' by default, otherwise use other env 
+rails_env = ENV['RAILS_ENV'] || 'staging'
+
+# Log everything to one file
+stderr_path "#{app_path}/log/unicorn.log"
+stdout_path "#{app_path}/log/unicorn.log"
+
+# Set master PID location
+pid "/home/gt/web/shared/pids/unicorn.pid"
+
+before_fork do |server, worker|
+  #ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  #ActiveRecord::Base.establish_connection
+end
