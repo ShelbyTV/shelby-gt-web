eb2085c8beff6bb743b367cb344fda94cbe54937
Michael Matyus <michael@maty.us>
2012-11-05 18:43:27 -0500

    disable activity preemption for iOS

--- app/assets/javascripts/app/utils/inactivity.utils.js
+++ app/assets/javascripts/app/utils/inactivity.utils.js
@@ -1,4 +1,4 @@
-if( typeof(shelby.userInactivity) === "undefined" ) shelby.userInactivity = {};
+if( !Browser.isIos() && typeof(shelby.userInactivity) === "undefined" ) shelby.userInactivity = {};
 
 $.extend(shelby.userInactivity, {
 
