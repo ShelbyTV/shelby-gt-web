e20b2705f7e359eda40623e93d72baca2c72d5cd
Joshua Samberg <josh@shelby.tv>
2012-11-08 14:01:18 -0500

    When deploying to staging update settings file with name of branch deployed

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -17,8 +17,21 @@ set :app_env,     "staging"
 require 'capistrano-unicorn'
 
 namespace :deploy do
+  desc "Write the name of the branch being deployed into a settings file that the app can use"
   task :write_branch_settings do
-    puts self[:deploy_to]
+    # load the default branch settings from file
+    branch_settings = YAML.load(ERB.new(open("config/settings/branch.yml").read).result).to_hash
+
+    # update the name of the branch for staging with the branch being deployed
+    branch_settings["staging"]["branch"] = branch
+
+    # write the change to the branch settings file on the server
+    yaml_lines = YAML::dump(branch_settings).split("\n")
+    branch_settings_file = "#{release_path}/config/settings/branch.yml"
+    run "cat /dev/null > #{branch_settings_file}"
+    yaml_lines.each do |line|
+      run "echo -e '#{line}' >> #{branch_settings_file}"
+    end
   end
 end
 
