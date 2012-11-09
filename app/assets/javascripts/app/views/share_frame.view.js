ee4cf5e5ec21b6b6257e053fea76d39c0c625ae2
Dan Spinosa <spinosa@gmail.com>
2012-11-09 15:22:19 -0500

    use discussion_roll route from share_frame view

--- app/assets/javascripts/app/views/share_frame.view.js
+++ app/assets/javascripts/app/views/share_frame.view.js
@@ -15,16 +15,56 @@
       messageCounter: false,
       shareButtonCopy: "Send"
     }),
+    
+    _share : function(){
+      var self = this;
 
-    saveUrl: function(){
-      return shelby.config.apiRoot + '/frame/' + this.options.frame.id + '/share';
+      if(!this._validateShare()) {
+        this.$('.js-share-textarea, .js-share-email-addresses').addClass('error');
+        this.onValidationFail();
+        return false;
+      }
+      
+      if (this.options.frame) {
+        libs.utils.rhombus.sadd('shares', this.options.frame.id);
+      }
+
+      this.$('.js-share-textarea').removeClass('error');
+      if (this._components.shareButton) {
+        this.$('.js-submit-share').addClass('js-sharing');
+      }
+      this._components.spinner && this._showSpinner();
+      
+      //adjust the model for what the POST discussion_roll route expects
+      this.model.set({
+        frame_id: this.options.frame.id,
+        participants: this.$('.js-share-email-addresses').val(),
+        message: this.model.get('text'),
+        destination: null,
+        text: null,
+        addresses: null
+        });
+      
+      //save adjusted model to discussion_roll route
+      this.model.save(null, {
+        url: shelby.config.apiRoot + '/discussion_roll',
+        success: function(){
+          self._clearTextArea();
+          self._components.spinner && self._hideSpinner();
+          self.onShareSuccess();
+        },
+        error: function(){
+          self._handleShareError();
+        }
+      });
+      return false;
     },
     
     onShareSuccess: function(){
       libs.shelbyGT.ShareView.prototype.onShareSuccess.call(this);
       
       shelby.track( 'shared_frame',
-                    { destination: this.model.get('destination').join(', '),
+                    { destination: 'discussion_roll',
                       id: this.options.frame.id,
                       userName: shelby.models.user.get('nickname')
                   });
