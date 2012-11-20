9224b658096b511d3c1567547318eda3edda76b5
Dan Spinosa <spinosa@gmail.com>
2012-11-19 19:15:12 -0500

    prepare app.html a little nicer

--- app/assets/javascripts/app/views/discussion_roll/standalone_discussion_roll.view.js
+++ app/assets/javascripts/app/views/discussion_roll/standalone_discussion_roll.view.js
@@ -1,22 +1,23 @@
 libs.shelbyGT.StandaloneDiscussionRollView = Support.CompositeView.extend({
   
-  //TODO: this probably changes
   el: '#js-shelby-wrapper',
   
   initialize : function(){
-    this._hackPrepareDOM();
+    //TODO: ideally, the styles applied to 'shelby-wrapper--discussion' should be applied to like... the body -MM
+    $('body').addClass('shelby-wrapper--discussion');
+    
     this.render();
     
-    //fetch the discussion roll
+    //fetch the discussion roll, which all my children are watching
     this.model.fetch({
       success: function(model, resp){
         setTimeout(function(){ $("body").scrollTop(10000000000); }, 100);
       },
       error: function(a){
-        //TODO: display error to user
-        console.log("ERROR on fetch of discussion roll. ", a);
+        $(".js-discussion-roll-conversation-list").html("<h1>Something went wrong :(</h1><h1>Try reloading...</h1>")
       }
     });
+
   },
   
   template : function(obj){
@@ -32,17 +33,6 @@ libs.shelbyGT.StandaloneDiscussionRollView = Support.CompositeView.extend({
     this.appendChildInto( new libs.shelbyGT.DiscussionRollConversationView(opts), 
       ".js-discussion-roll-conversation-wrapper");
     this.renderChild(     new libs.shelbyGT.DiscussionRollReplyView(opts));
-  },
-  
-  //XXX don't actualy want to do this in production
-  //    what we should do: have the wrapper empty in app.html
-  //    and load a top-level layout view into it from the router
-  //    MM will have input there
-  _hackPrepareDOM : function(){
-    //TODO: should just display different layout for entire page; quick & dirty hack for now...
-    $("#js-shelby-wrapper").empty();
-    $('body').addClass('shelby-wrapper--discussion');
-    //ideally, the styles applied to 'shelby-wrapper--discussion' should be applied to like.. the body
   }
   
 });
--- app/controllers/discussion_roll_controller.rb
+++ app/controllers/discussion_roll_controller.rb
@@ -10,6 +10,7 @@ class DiscussionRollController < ApplicationController
   def show
     # TODO: could do some clever shit if the user was logged in vs not
     #       but don't need to.  KISS for now.  JS needs to handle all cases anyway, i think.
+    @empty_shelby_wrapper = true
     render '/home/app'
   end
   
