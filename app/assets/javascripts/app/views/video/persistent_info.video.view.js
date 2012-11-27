0d2be0b5213b684bf3e36a994da310269d06026d
Michael Matyus <michael@maty.us>
2012-11-27 18:45:21 -0500

    first swipe at hot switch stylingz

--- app/assets/javascripts/app/views/video/persistent_info.video.view.js
+++ app/assets/javascripts/app/views/video/persistent_info.video.view.js
@@ -4,34 +4,34 @@
  *
  */
 libs.shelbyGT.PersistentVideoInfoView = Support.CompositeView.extend({
-  
+
   events : {
-    "click .persistent_video_info--current-frame  .js-roll-frame"                 : "_requestCurrentFrameRollView",
-    "click .persistent_video_info--next-frame     .js-roll-frame"                 : "_requestNextFrameRollView",
-    "click .persistent_video_info--current-frame  .js-share-frame"                : "_requestCurrentFrameShareView",
-    "click .persistent_video_info--next-frame     .js-share-frame"                : "_requestNextFrameShareView",
-    "click .persistent_video_info--current-frame  .js-queue-frame:not(.queued)"   : "_queueCurrentFrame",
-    "click .persistent_video_info--next-frame     .js-queue-frame:not(.queued)"   : "_queueNextFrame"
+    "click .persistent_video_info__current-frame  .js-roll-frame"                 : "_requestCurrentFrameRollView",
+    "click .persistent_video_info__next-frame     .js-roll-frame"                 : "_requestNextFrameRollView",
+    "click .persistent_video_info__current-frame  .js-share-frame"                : "_requestCurrentFrameShareView",
+    "click .persistent_video_info__next-frame     .js-share-frame"                : "_requestNextFrameShareView",
+    "click .persistent_video_info__current-frame  .js-queue-frame:not(.queued)"   : "_queueCurrentFrame",
+    "click .persistent_video_info__next-frame     .js-queue-frame:not(.queued)"   : "_queueNextFrame"
   },
 
   initialize: function(opts){
     this.options.guide.bind('change:activeFrameModel', this._onActiveFrameModelChange, this);
     Backbone.Events.bind("change:playingFrameGroupCollection", this._onPlayingFrameGroupCollectionChange, this);
   },
-  
+
   _cleanup : function() {
     this.options.guide.unbind('change:activeFrameModel', this._onActiveFrameModelChange, this);
     Backbone.Events.unbind("change:playingFrameGroupCollection", this._onPlayingFrameGroupCollectionChange, this);
   },
-  
-  template : function(obj) { 
+
+  template : function(obj) {
     try {
       return SHELBYJST['video/persistent-video-info'](obj);
     } catch(e){
       console.log(e.message, e.stack, obj);
     }
   },
-  
+
   render : function() {
     if(this._currentFrame && this._playingFrameGroupCollection){
       this._nextFrame = this._playingFrameGroupCollection.getNextPlayableFrame(this._currentFrame, 1, true);
@@ -45,43 +45,61 @@ libs.shelbyGT.PersistentVideoInfoView = Support.CompositeView.extend({
         }));
     }
   },
-  
+
   _onActiveFrameModelChange : function(guideModel, activeFrameModel){
     this._currentFrame = activeFrameModel;
     this.render();
   },
-  
+
   _onPlayingFrameGroupCollectionChange : function(playingFrameGroupCollection){
     this._playingFrameGroupCollection = playingFrameGroupCollection;
     this.render();
   },
-  
+
   /*************************************************************
    * Button events
    *************************************************************/
-  _requestCurrentFrameRollView : function(){ this._requestFrameRollView(this._currentFrame); },
-  _requestNextFrameRollView : function(){ this._requestFrameRollView(this._nextFrame); },
+  _requestCurrentFrameRollView : function(){
+    this._requestFrameRollView(this._currentFrame);
+  },
+
+  _requestNextFrameRollView : function(){
+    this._requestFrameRollView(this._nextFrame);
+  },
+
   _requestFrameRollView : function(frame){
     if( shelby.views.anonBanner.userIsAbleTo(libs.shelbyGT.AnonymousActions.ROLL) ){
       this.options.guideOverlayModel.switchOrHideOverlay(libs.shelbyGT.GuideOverlayType.rolling, frame);
     }
   },
-  
-  _requestCurrentFrameShareView : function(){ this._requestFrameShareView(this._currentFrame); },
-  _requestNextFrameShareView : function(){ this._requestFrameShareView(this._nextFrame); },
+
+  _requestCurrentFrameShareView : function(){
+    this._requestFrameShareView(this._currentFrame);
+  },
+
+  _requestNextFrameShareView : function(){
+    this._requestFrameShareView(this._nextFrame);
+  },
+
   _requestFrameShareView: function(frame){
     if( shelby.views.anonBanner.userIsAbleTo(libs.shelbyGT.AnonymousActions.COMMENT) ){
       this.options.guideOverlayModel.switchOrHideOverlay(libs.shelbyGT.GuideOverlayType.share, frame);
     }
   },
-  
-  _queueCurrentFrame : function(el){ this._queueFrame(this._currentFrame, el); },
-  _queueNextFrame : function(el){ this._queueFrame(this._nextFrame, el); },
+
+  _queueCurrentFrame : function(el){
+    this._queueFrame(this._currentFrame, el);
+  },
+
+  _queueNextFrame : function(el){
+    this._queueFrame(this._nextFrame, el);
+  },
+
   _queueFrame : function(frame, el){
     if( shelby.views.anonBanner.userIsAbleTo(libs.shelbyGT.AnonymousActions.QUEUE) ){
       frame.saveToWatchLater();
       $(el.currentTarget).toggleClass('button_gray-light queued js-queued').find('.js-command-icon').text('Queued');
     }
   }
-  
+
 });
