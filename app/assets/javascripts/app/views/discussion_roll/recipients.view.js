c64a5f901e5cd0a5de748c09533b0c15c3beed76
Dan Spinosa <spinosa@gmail.com>
2012-11-27 13:53:30 -0500

    hide overflow recipients behind a more link

--- app/assets/javascripts/app/views/discussion_roll/recipients.view.js
+++ app/assets/javascripts/app/views/discussion_roll/recipients.view.js
@@ -4,6 +4,13 @@
  */
 libs.shelbyGT.DiscussionRollRecipientsView = Support.CompositeView.extend({
 
+  //when we have more than OVERFLOW_AT recipients, show a "more" link instead of the full list initially
+  OVERFLOW_AT: 2,
+
+  events: {
+    "click .js-discussion-roll-recipients-toggle-show-all"  : "_toggleShowAllRecipients"
+  },
+  
   el: '.js-discussion-roll-recipients',
   
   initialize : function(){
@@ -27,16 +34,30 @@ libs.shelbyGT.DiscussionRollRecipientsView = Support.CompositeView.extend({
     
     this.$el.html(this.template());
     
-    if(this.model.get('discussion_roll_participants')){ 
+    if(this.model.get('discussion_roll_participants')){
+      var i = 0;
+      
       this.model.get('discussion_roll_participants').forEach(function(p){ 
         //show who this is TO (don't include the current viewer in that list)
         if( self.options.viewer !== p ){
           self.appendChildInto(
-            new libs.shelbyGT.DiscussionRollRecipientView({model:p, discussionRoll:self.model}),
+            new libs.shelbyGT.DiscussionRollRecipientView({model:p, discussionRoll:self.model, overflow:(i>=self.OVERFLOW_AT)}),
             '.js-discussion-roll-recipients-list' );
+          i++;
         }
       });
+      
+      if(i>=this.OVERFLOW_AT){
+        this.$('.js-discussion-roll-recipients-toggle-show-all').show();
+      }
     }
+
+  },
+  
+  _toggleShowAllRecipients: function(e){
+    e.preventDefault();
+    
+    this.$el.toggleClass("show-all-recipients");
   }
   
 });
