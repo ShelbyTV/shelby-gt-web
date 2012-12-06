13700b4f2a86e36bf2a7700b829f911d42f23a95
Joshua Samberg <josh@shelby.tv>
2012-12-06 13:42:20 -0500

    Add capitalize to the list of underscore.string functions mixed into underscore

--- app/assets/javascripts/backbone/underscore-custom-setup.js
+++ app/assets/javascripts/backbone/underscore-custom-setup.js
@@ -1,2 +1,5 @@
 //mixin the underscore.string functions we want so they can be used with underscore's OO syntax
-_.mixin({clean: _.str.clean});
+_.mixin({
+  capitalize: _.str.capitalize,
+  clean: _.str.clean
+});
