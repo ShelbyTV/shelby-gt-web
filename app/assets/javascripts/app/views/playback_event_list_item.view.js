3eae8576b177a6a30923177390e9015798e856b6
Joshua Samberg <josh@shelby.tv>
2012-12-13 17:24:03 -0500

    Completed implementation of popup themes

--- app/assets/javascripts/app/views/playback_event_list_item.view.js
+++ app/assets/javascripts/app/views/playback_event_list_item.view.js
@@ -23,7 +23,7 @@ libs.shelbyGT.PlaybackEventListItemView = libs.shelbyGT.ListItemView.extend({
       end_time : libs.utils.Time.HMStoS(+this.$('#end-hours-' + this.options.index).val(), +this.$('#end-minutes-' + this.options.index).val(), +this.$('#end-seconds-' + this.options.index).val()),
       html : this.$('.js-event-html').val(),
       start_time : libs.utils.Time.HMStoS(+this.$('#start-hours-' + this.options.index).val(), +this.$('#start-minutes-' + this.options.index).val(), +this.$('#start-seconds-' + this.options.index).val()),
-      theme : this.$('.js-theme-select').val()
+      theme : +this.$('.js-theme-select').val()
     });
   }
 
