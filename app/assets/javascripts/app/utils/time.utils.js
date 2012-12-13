380e3209f683f40e1e7ba832a5f5c0d363cfbd54
Joshua Samberg <josh@shelby.tv>
2012-12-13 16:45:29 -0500

    Event list item views for existing items render the saved data

--- app/assets/javascripts/app/utils/time.utils.js
+++ app/assets/javascripts/app/utils/time.utils.js
@@ -2,6 +2,14 @@ libs.utils.Time = {
   
   HMStoS : function(hours, minutes, seconds){
     return (hours * 60 * 60) + (minutes * 60) + seconds;
+  },
+
+  StoHMS : function(seconds){
+    return {
+      hours : parseInt(seconds / (60*60), 10 ) % 60,
+      minutes : parseInt(seconds / 60, 10 ) % 60,
+      seconds : parseInt(seconds % 60, 10)
+    };
   }
 
  };
--- app/assets/javascripts/app/views/playback_event_list_item.view.js
+++ app/assets/javascripts/app/views/playback_event_list_item.view.js
@@ -1,16 +1,18 @@
 libs.shelbyGT.PlaybackEventListItemView = libs.shelbyGT.ListItemView.extend({
-  
+
   events : {
 
   },
-    
+
   template: function(obj){
     return SHELBYJST['popup-event-item'](obj);
   },
-  
+
   render: function(){
     this.$el.html(this.template({
       event : this.model,
+      eventStartTime: libs.utils.Time.StoHMS(this.model.get('start_time')),
+      eventEndTime: libs.utils.Time.StoHMS(this.model.get('end_time')),
       index : this.options.index
     }));
   },
@@ -22,5 +24,5 @@ libs.shelbyGT.PlaybackEventListItemView = libs.shelbyGT.ListItemView.extend({
       start_time : libs.utils.Time.HMStoS(+this.$('#start-hours-' + this.options.index).val(), +this.$('#start-minutes-' + this.options.index).val(), +this.$('#start-seconds-' + this.options.index).val())
     });
   }
-  
+
 });
