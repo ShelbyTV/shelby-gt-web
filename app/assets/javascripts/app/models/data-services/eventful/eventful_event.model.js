137de79165925e0bd68fe206c7da6838426589d5
Michael Matyus <michael@maty.us>
2012-12-14 14:21:19 -0500

    shitty, pre-pull commit

--- app/assets/javascripts/app/models/data-services/eventful/eventful_event.model.js
+++ app/assets/javascripts/app/models/data-services/eventful/eventful_event.model.js
@@ -1,10 +1,13 @@
 EventfulEventModel = Backbone.Model.extend({
   defaults : {
     city_name : 'New York City',
+    region_abbr : 'NY',
     country_abbr2 : 'US',
-    performers : [{
-      name : 'Prince'
-    }],
+    performers : {
+      performer : [{
+        name : 'Shelby Boyz'
+      }]
+    },
     price : '$25.00',
     start_time : '2013-04-11 20:00:00',
     venue_name : 'The Creamery'
--- app/assets/javascripts/app/views/video/persistent_info.video.view.js
+++ app/assets/javascripts/app/views/video/persistent_info.video.view.js
@@ -144,8 +144,18 @@ libs.shelbyGT.PersistentVideoInfoView = Support.CompositeView.extend({
     shelby.collections.eventfulEventCollection.reset([
     {
       city_name : 'New York City',
-      venue_name : 'CBGB',
-      price : '$100'
+      region_abbr : 'NY',
+      country_abbr2 : 'US',
+      performers : {
+        performer : [
+          {
+            name : 'Shelby Boyz'
+          }
+        ]
+      },
+      price : '$25.00',
+      start_time : '2013-04-11 20:00:00',
+      venue_name : 'The Creamery'
     },{
       city_name : 'Augusta',
       venue_name : 'Augustorama'
