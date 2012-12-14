66b8e76d9f2c6cf65cf7505ba9cd21833430059f
Joshua Samberg <josh@shelby.tv>
2012-12-14 16:11:37 -0500

    Sort concert results by relevance

--- app/assets/javascripts/app/views/video/persistent_info.video.view.js
+++ app/assets/javascripts/app/views/video/persistent_info.video.view.js
@@ -148,7 +148,6 @@ libs.shelbyGT.PersistentVideoInfoView = Support.CompositeView.extend({
       app_key : 'fs6tqWdGP5Cn5zvP',
       q : event.get('concert_query') || this._currentFrame.get('video').get('title'),
       page_size : 25,
-      sort_order : 'date',
       include : "links, price"
     };
 
