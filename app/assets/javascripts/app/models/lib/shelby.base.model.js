bf3569dddddc75cb6b9b8c9d86fe936c1d4ffe86
Joshua Samberg <josh@shelby.tv>
2012-12-21 11:36:59 -0500

    Don't try to use a nonexistant jqXHR if fetch gives us one

--- app/assets/javascripts/app/models/lib/shelby.base.model.js
+++ app/assets/javascripts/app/models/lib/shelby.base.model.js
@@ -5,29 +5,29 @@ libs.shelbyGT.ShelbyBaseModel = Backbone.RelationalModel.extend({
   parse : function (response) {
     return response.result;
   },
-  
+
   methodMap : {
     'create': 'POST',
     'update': 'PUT',
     'delete': 'DELETE',
     'read':   'GET'
   },
-  
+
   sync : function(method, model, options) {
-    
+
     if( !Browser.supportsCORS() ){
       //send as JSONP
       overrides = {
         dataType: 'jsonp',
         contentType: 'application/x-www-form-urlencoded'
         };
-        
+
       //re-encode the model properly for the URL
       if (model && (method == 'create' || method == 'update')) {
         overrides.data = $.param(model.toJSON());
       }
       _.extend(options, overrides);
-    
+
       // dynamically re-write the route to append POST/ PUT/ DELETE/ after v1/
       if (!options.url) {
         options.url = _.isFunction(model.url) ? model.url() : model.url;
@@ -37,24 +37,26 @@ libs.shelbyGT.ShelbyBaseModel = Backbone.RelationalModel.extend({
         options.url = options.url.slice(0,loc+3) + "/" + this.methodMap[method] + options.url.slice(loc+3);
       }
     }
-    
+
     return Backbone.sync(method, model, options);
   },
-  
+
   // supporting the following additional events (in addition to those of Backbone)
   messages : {
     fetchComplete : "fetch:complete",
   },
-  
+
   fetch : function(options){
     var self = this;
     var jqXHR = Backbone.Model.prototype.fetch.call(this, options);
-    jqXHR.success(function(resp){
-      self.trigger(self.messages.fetchComplete);
-    });
+    if (jqXHR) {
+      jqXHR.success(function(resp){
+        self.trigger(self.messages.fetchComplete);
+      });
+    }
     return jqXHR;
   },
-  
+
   /*  method: triggerTransientChange
       description: Conveniece method to set model attribute, in the process executing all change handlers bound and
         scoped to that attribute, then reset the attribute to null. Allows a stateful approach to sending
@@ -64,7 +66,7 @@ libs.shelbyGT.ShelbyBaseModel = Backbone.RelationalModel.extend({
     this.set(attribute, value);
     this.set(attribute, null);
   },
-  
+
   hasBsonId: function(){
     //should be a 24 character
     if(this.id.length !== 24){ return false; }
