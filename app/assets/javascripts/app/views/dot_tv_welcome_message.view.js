c2f16c71f44e4eeb21da5f9142fb352854557b3b
Michael Matyus <michael@maty.us>
2012-12-28 15:31:39 -0500

    nitpicky tweaks

--- app/assets/javascripts/app/views/dot_tv_welcome_message.view.js
+++ app/assets/javascripts/app/views/dot_tv_welcome_message.view.js
@@ -25,7 +25,7 @@ libs.shelbyGT.dotTVWelcome = Support.CompositeView.extend({
   },
 
   _startPlaying : function(){
-    $('#dot-tv-welcome-message').addClass('hidden');
+    this.$el.addClass('hidden');
     // now play the video and reset autoplay to true.
     shelby.models.userDesires.triggerTransientChange('playbackStatus', libs.shelbyGT.PlaybackStatus.playing);
     shelby.models.playbackState.set('autoplayOnVideoDisplay', true);
