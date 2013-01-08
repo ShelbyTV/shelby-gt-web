21672e6a1bb6c033ff5cb12dcd052d1176163178
Joshua Samberg <josh@shelby.tv>
2013-01-08 16:33:34 -0500

    Fixed issue with search forms encoding spaces as + and router not decoding

--- app/assets/javascripts/backbone/backbone.queryparams.js
+++ app/assets/javascripts/backbone/backbone.queryparams.js
@@ -1,35 +1,102 @@
-(function() {
+(function(_, Backbone) {
 
-var queryStringParam = /^\?(.*)/;
-var namedParam    = /:([\w\d]+)/g;
-var splatParam    = /\*([\w\d]+)/g;
-var escapeRegExp  = /[-[\]{}()+?.,\\^$|#\s]/g;
+// Require Underscore and Backbone if there's a `require` function.
+// This makes `backbone.queryparam` work on the server or when using
+// `browserify`.
+if (typeof require !== 'undefined') {
+  _ = _ || require('underscore');
+  Backbone = Backbone || require('backbone');
+}
 
-_.extend(Backbone.Router.prototype, {
+var queryStringParam = /^\?(.*)/,
+    optionalParam = /\((.*?)\)/g,
+    namedParam    = /(\(\?)?:\w+/g,
+    splatParam    = /\*\w+/g,
+    escapeRegExp  = /[\-{}\[\]+?.,\\\^$|#\s]/g,
+    queryStrip = /(\?.*)$/,
+    fragmentStrip = /^([^\?]*)/,
+    namesPattern = /[\:\*]([^\:\?\/]+)/g;
+Backbone.Router.arrayValueSplit = '|';
+Backbone.Router.decodeFragment = true;
+
+var _getFragment = Backbone.History.prototype.getFragment;
+
+_.extend(Backbone.History.prototype, {
   getFragment : function(fragment, forcePushState, excludeQueryString) {
-    if (fragment == null) {
-      if (this._hasPushState || forcePushState) {
-        fragment = window.location.pathname;
-        var search = window.location.search;
-        if (search) fragment += search;
-      } else {
-        fragment = window.location.hash;
-      }
-    }
-    fragment = fragment.replace(hashStrip, '');
+    fragment = _getFragment.apply(this, arguments);
     if (excludeQueryString) {
       fragment = fragment.replace(queryStrip, '');
     }
-    if (!fragment.indexOf(this.options.root)) fragment = fragment.substr(this.options.root.length);
     return fragment;
   },
-  
-  _routeToRegExp : function(route) {
-    route = route.replace(escapeRegExp, "\\$&")
-                 .replace(namedParam, "([^\/?]*)")
-                 .replace(splatParam, "([^\?]*)");
+
+  // this will not perform custom query param serialization specific to the router
+  // but will return a map of key/value pairs (the value is a string or array)
+  getQueryParameters : function(fragment, forcePushState) {
+    fragment = _getFragment.apply(this, arguments);
+    // if no query string exists, this will still be the original fragment
+    var queryString = fragment.replace(fragmentStrip, '');
+    var match = queryString.match(queryStringParam);
+    if (match) {
+      queryString = match[1];
+      var rtn = {};
+      iterateQueryString(queryString, function(name, value) {
+        if (!rtn[name]) {
+          rtn[name] = value;
+        } else if (_.isString(rtn[name])) {
+          rtn[name] = [rtn[name], value];
+        } else {
+          rtn[name].push(value);
+        }
+      });
+      return rtn;
+    } else {
+      // no values
+      return {};
+    }
+  }
+});
+
+_.extend(Backbone.Router.prototype, {
+  initialize: function(options) {
+    this.encodedSplatParts = options && options.encodedSplatParts;
+  },
+
+  getFragment : function(fragment, forcePushState, excludeQueryString) {
+    fragment = _getFragment.apply(this, arguments);
+    if (excludeQueryString) {
+      fragment = fragment.replace(queryStrip, '');
+    }
+    return fragment;
+  },
+
+  _routeToRegExp: function(route) {
+    var splatMatch = (splatParam.exec(route) || {index: -1}),
+        namedMatch = (namedParam.exec(route) || {index: -1}),
+        paramNames = route.match(namesPattern) || [];
+
+    route = route.replace(escapeRegExp, '\\$&')
+                 .replace(optionalParam, '(?:$1)?')
+                 .replace(namedParam, function(match, optional){
+                   return optional ? match : '([^\\/\\?]+)';
+                 })
+                 .replace(splatParam, '([^\?]*?)');
     route += '([\?]{1}.*)?';
-    return new RegExp('^' + route + '$');
+    var rtn = new RegExp('^' + route + '$');
+
+  // use the rtn value to hold some parameter data
+  if (splatMatch.index >= 0) {
+    // there is a splat
+    if (namedMatch >= 0) {
+      // negative value will indicate there is a splat match before any named matches
+      rtn.splatMatch = splatMatch.index - namedMatch.index;
+    } else {
+      rtn.splatMatch = -1;
+    }
+  }
+  rtn.paramNames = paramNames.map(function(name) { return name.substring(1); });
+
+  return rtn;
   },
 
   /**
@@ -37,7 +104,8 @@ _.extend(Backbone.Router.prototype, {
    * extracted parameters.
    */
   _extractParameters : function(route, fragment) {
-    var params = route.exec(fragment).slice(1);
+    var params = route.exec(fragment).slice(1),
+        namedParams = {};
 
     // do we have an additional query string?
     var match = params.length && params[params.length-1] && params[params.length-1].match(queryStringParam);
@@ -45,26 +113,36 @@ _.extend(Backbone.Router.prototype, {
       var queryString = match[1];
       var data = {};
       if (queryString) {
-        var keyValues = queryString.split('&');
         var self = this;
-        _.each(keyValues, function(keyValue) {
-          var arr = keyValue.split('=');
-          if (arr.length > 1 && arr[1]) {
-            self._setParamValue(arr[0], arr[1], data);
-          }
+        iterateQueryString(queryString, function(name, value) {
+          self._setParamValue(name, value, data);
         });
       }
       params[params.length-1] = data;
+      _.extend(namedParams, data);
     }
 
     // decode params
-    for (var i=0; i<params.length; i++) {
-      if (_.isString(params[i])) {
-        params[i] = decodeURIComponent(params[i]);
+    var length = params.length;
+    if (route.splatMatch && this.encodedSplatParts) {
+      if (route.splatMatch < 0) {
+        // splat param is first
+        return params;
+      } else {
+        length = length - 1;
       }
     }
 
-    return params;
+    for (var i=0; i<length; i++) {
+      if (_.isString(params[i])) {
+        params[i] = Backbone.Router.decodeParams ? decodeURIComponent(params[i]) : params[i];
+        if (route.paramNames.length >= i-1) {
+          namedParams[route.paramNames[i]] = params[i];
+        }
+      }
+    }
+
+    return Backbone.Router.namedParameters ? [namedParams] : params;
   },
 
   /**
@@ -92,25 +170,26 @@ _.extend(Backbone.Router.prototype, {
    */
   _decodeParamValue : function(value, currentValue) {
     // '|' will indicate an array.  Array with 1 value is a=|b - multiple values can be a=b|c
-    if (value.indexOf('|') >= 0) {
-      var values = value.split('|');
+    var splitChar = Backbone.Router.arrayValueSplit;
+    if (value.indexOf(splitChar) >= 0) {
+      var values = value.split(splitChar);
       // clean it up
       for (var i=values.length-1; i>=0; i--) {
         if (!values[i]) {
           values.splice(i, 1);
         } else {
-          values[i] = decodeURIComponent(values[i])
+          values[i] = Backbone.Router.decodeFragment ? decodeQueryComponent(values[i]) : values[i];
         }
       }
       return values;
     }
     if (!currentValue) {
-      return decodeURIComponent(value);
+      return Backbone.Router.decodeFragment ? decodeQueryComponent(value) : value;
     } else if (_.isArray(currentValue)) {
-      currentValue.push(value);
+      currentValue.push(Backbone.Router.decodeFragment ? decodeQueryComponent(value) : value);
       return currentValue;
     } else {
-      return [currentValue, value];
+      return [currentValue, Backbone.Router.decodeFragment ? decodeQueryComponent(value) : value];
     }
   },
 
@@ -122,7 +201,9 @@ _.extend(Backbone.Router.prototype, {
       if (!_.isString(queryParameters)) {
         queryParameters = this._toQueryString(queryParameters);
       }
-      route += '?' + queryParameters;
+      if(queryParameters) {
+        route += '?' + queryParameters;
+      }
     }
     return route;
   },
@@ -131,24 +212,27 @@ _.extend(Backbone.Router.prototype, {
    * Serialize the val hash to query parameters and return it.  Use the namePrefix to prefix all param names (for recursion)
    */
   _toQueryString: function(val, namePrefix) {
+    var splitChar = Backbone.Router.arrayValueSplit;
+    function encodeSplit(val) { return val.replace(splitChar, encodeURIComponent(splitChar)); }
+
     if (!val) return '';
     namePrefix = namePrefix || '';
     var rtn = '';
     for (var name in val) {
       var _val = val[name];
       if (_.isString(_val) || _.isNumber(_val) || _.isBoolean(_val) || _.isDate(_val)) {
-        // primitave type
+        // primitive type
         _val = this._toQueryParam(_val);
-        if (_.isBoolean(_val) || _val) {
-          rtn += (rtn ? '&' : '') + this._toQueryParamName(name, namePrefix) + '=' + encodeURIComponent(_val).replace('|', '%7C');
+        if (_.isBoolean(_val) || _.isNumber(_val) || _val) {
+          rtn += (rtn ? '&' : '') + this._toQueryParamName(name, namePrefix) + '=' + encodeSplit(encodeURIComponent(_val));
         }
       } else if (_.isArray(_val)) {
-        // arrrays use | separator
+        // arrays use Backbone.Router.arrayValueSplit separator
         var str = '';
         for (var i in _val) {
           var param = this._toQueryParam(_val[i]);
           if (_.isBoolean(param) || param) {
-            str += '|' + encodeURIComponent(param).replace('|', '%7C');
+            str += splitChar + encodeSplit(param);
           }
         }
         if (str) {
@@ -188,4 +272,21 @@ _.extend(Backbone.Router.prototype, {
     return param;
   }
 });
-})();
+
+function iterateQueryString(queryString, callback) {
+  var keyValues = queryString.split('&');
+  _.each(keyValues, function(keyValue) {
+    var i = keyValue.indexOf('=');
+    var arr = [keyValue.slice(0,i), keyValue.slice(i+1)];
+    if (arr.length > 1) {
+      callback(arr[0], arr[1]);
+    }
+  });
+}
+
+function decodeQueryComponent(encodedComponent) {
+  // handle spaces that are encoded as + in the query, which decodeURICompoent doesn't do
+  return decodeURIComponent((encodedComponent+'').replace(/\+/g, '%20'));
+}
+
+})(_, Backbone);
