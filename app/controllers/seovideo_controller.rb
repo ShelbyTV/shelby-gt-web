81655284ed96f3b8725a9ae8236334980baf13b5
Joshua Samberg <josh@shelby.tv>
2013-01-14 13:10:26 -0800

    Don't need to manually remove all invalid utf-8 characters from search query - our rails fork is doing that for us now

--- app/controllers/seovideo_controller.rb
+++ app/controllers/seovideo_controller.rb
@@ -68,15 +68,9 @@ class SeovideoController < ApplicationController
               # check if the google url specified an encoding for the search query and if so decode it accordingly
               if search_query_encoding = query_values["ie"]
                 if encoding_obj = Encoding.list.find {|enc| enc.name.casecmp(search_query_encoding) == 0 }
-                  # re-encode the search query as UTF-8 so ActiveSupport doesn't choke on it
+                  # re-encode the search query as UTF-8, respecting the input encoding that was passed to google
                   @search_query.encode!('utf-8', encoding_obj, :invalid => :replace, :undef => :replace, :replace => '?')
-                else
-                  # change any characters that can't be encoded in UTF-8 into ?'s
-                  @search_query.encode!('utf-8', 'binary', :invalid => :replace, :undef => :replace, :replace => '?')
                 end
-              else
-                # change any characters that can't be encoded in UTF-8 into ?'s
-                @search_query.encode!('utf-8', 'binary', :invalid => :replace, :undef => :replace, :replace => '?')
               end
             end
           end
