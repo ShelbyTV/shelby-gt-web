b4ad76747cd939384fbbf39f622cf9a1fd0842ac
Henry <henry@sztul.com>
2013-01-15 13:44:26 -0500

    added a few rolls to channels

--- app/assets/javascripts/app/config/multiplexed_video.config.js
+++ app/assets/javascripts/app/config/multiplexed_video.config.js
@@ -1,10 +1,10 @@
 shelby.config.multiplexedVideoRolls = {
-  // espn, redbull, kicktv, gopro
-  play: ["4fbe42069a725b686300004a","4fb413bb9a725b650900195a","4f95db0388ba6b6125004073", "4f91700d88ba6b2aab000fba"],
+  // espn, redbull, kicktv, gopro, banffmtfestival
+  play: ["4fbe42069a725b686300004a","4fb413bb9a725b650900195a","4f95db0388ba6b6125004073", "4f91700d88ba6b2aab000fba", "4f9012e8b415cc466a000868"],
   // reuters, nytimesvideo, wsj, ytpolitics, aljazeera english
   inform: ["4f95622b9a725b72e700106b","50522466b415cc5b81002080", "4fa8542c88ba6b669b000bcd", "4fa05c799a725b020a000a5e", "50d083b1b415cc73fd00e89f"],
-  // laughingsquid, dailywhat, chow, onion, snl shorts, harto, buzzfeed,
-  giggle: ["4fc637879a725b755d001f77","4fa28d849a725b79c9000228","50072ee988ba6b796b00e4fd", "4fc532169a725b2999000354", "4fbd2d24d10412081e00042e","4f902316b415cc466a0009dd","4f9577c488ba6b15ec0084c7"],
+  // laughingsquid, dailywhat, chow, onion, snl shorts, harto, buzzfeed, jakeandamir, collegehumor
+  giggle: ["4fc637879a725b755d001f77","4fa28d849a725b79c9000228","50072ee988ba6b796b00e4fd", "4fc532169a725b2999000354", "4fbd2d24d10412081e00042e","4f902316b415cc466a0009dd","4f9577c488ba6b15ec0084c7", "4fb3d39988ba6b33460043bb", "4f9009d9b415cc466a000466"],
   // minute physics, khan acad., pbs , brainpicker, tedtalksvideo, neiltyson, crash course
   inspire: ["4f9f3fc19a725b5b9700329b","4fa39bd89a725b1f920008f3", "4faabca99a725b5cd50026a2","4fbad2c5d1041266bc000d4f","4fbaa51d1c1cf44b9d002f58", "4fcce43988ba6b5b7400054a", "4fb5a66a9a725b3af0005b66"],
   // henry, reece, spinosa, chris, vondoom, art, matyus, iceberg901, bcoffey
