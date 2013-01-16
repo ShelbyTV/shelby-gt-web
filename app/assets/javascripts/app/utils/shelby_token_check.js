aa04e06969d5713f72aab3723c4672c002c9c523
Michael Matyus <michael@maty.us>
2013-01-16 10:19:31 -0500

    alert formatting for FB auth, fixes #792

--- app/assets/javascripts/app/utils/shelby_token_check.js
+++ app/assets/javascripts/app/utils/shelby_token_check.js
@@ -1,38 +1,39 @@
 _(shelby).extend({
-	userHasProvider : function(provider){
-		// check if user has an authentication
-		var hasProvider = false;
-		$(shelby.models.user.get('authentications')).each(function(i,auth){
-			if (auth.provider == provider) { hasProvider = true; }
-		});
-		return hasProvider;
-	},
+  userHasProvider : function(provider){
+    // check if user has an authentication
+    var hasProvider = false;
+    $(shelby.models.user.get('authentications')).each(function(i,auth){
+      if (auth.provider == provider) { hasProvider = true; }
+    });
+    return hasProvider;
+  },
 
-	checkFbTokenValidity : function(){
-		if (shelby.userHasProvider('facebook')){
-			// do ajax request to see if token is valid
-			var _url = shelby.config.apiRoot + '/user/' +  shelby.models.user.id + '/is_token_valid?provider=facebook';
-			var _fbAuth = shelby.config.apiBase + "/auth/facebook";
-			$.ajax({ url:_url, xhrFields: {withCredentials: true} })
-				.success(function(r){
-					if (r.status == 200 && !r.result.token_valid){
-						shelby.alert({
-							message: "In order to provide a great experience we need you to re-authenticate with facebook. Don't worry, you'll be re-directed back to Shelby. Yay!",
-							primary: {
-								title: 'Re-authenticate Facebook',
-								route: _fbAuth
-							},
-							secondary: {
-								title: 'No thanks'
-							}
-						},
-						function(){ document.location.href = _fbAuth; });
-					}
-				})
-				.error(function(r){
-					console.log("error checking fb credentials");
-				});
-		}
-	}
+  checkFbTokenValidity : function(){
+    if (shelby.userHasProvider('facebook')){
+      // do ajax request to see if token is valid
+      var _url = shelby.config.apiRoot + '/user/' +  shelby.models.user.id + '/is_token_valid?provider=facebook';
+      var _fbAuth = shelby.config.apiBase + "/auth/facebook";
+      $.ajax({ url:_url, xhrFields: {withCredentials: true} })
+        .success(function(r){
+          if (r.status == 200 && !r.result.token_valid){
+            shelby.alert({
+              message: "In order to provide a great experience we need you to re-authenticate with facebook. Don't worry, you'll be re-directed back to Shelby. Yay!",
+              primary: {
+                title: 'Re-authenticate Facebook',
+                route: _fbAuth
+                },
+              secondary: {
+                title: 'No thanks'
+                }
+              },
+              function(){ document.location.href = _fbAuth; }
+            );
+        }
+      })
+      .error(function(r){
+        console.log("error checking fb credentials");
+      });
+    }
+  }
 });
 
