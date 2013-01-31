19fff797062b1c70da4c65f166b5f0edaaea3041
Dan Spinosa <spinosa@gmail.com>
2013-01-31 17:10:10 -0500

    full integration from host page JS to web server to API and back.  Just need to render actual embellishments

---
+++ app/assets/javascripts/turbo.js
@@ -0,0 +1,3 @@
+//This is loaded by our small embed snippet on a host page.
+//That snippet looks something like the contents of ./turbo-embed/turbo-bootstrap.html.erb
+//= require ./turbo-embed/turbo.js.erb
--- app/controllers/turbo_embed_controller.rb
+++ app/controllers/turbo_embed_controller.rb
@@ -1,5 +1,6 @@
-class TurboEmbedController < ApplicationController
-  
+require 'shelby_api'
+
+class TurboEmbedController < ApplicationController  
 
   # GET /turbo_embellish
   #
@@ -12,14 +13,32 @@ class TurboEmbedController < ApplicationController
   # Otherwise, we are doing nothing for now.
   #
   # required params:
-  #  nick = The User's nickname for the public roll we care about
+  #  userId = The User's id for the public roll we care about
   #  providerName = The name of the video provider for the given embed
   #  videoId = the unique id of the given video at the given provider
   #  shelbyTurboTag = The given video embed has an attribute shelby-turbo-tag on the host page.
   #                   It is set to this value.  Our returned JS directly updates the page.
   #
   def embellish
-    #TODO
+    # Get the given user and the frames on their public roll
+    @user = Shelby::API.get_user(params[:userId])
+    @roll = Shelby::API.get_roll_with_frames(@user['personal_roll_id']) if @user
+    
+    render :nothing => true unless @user and @roll
+    
+    @embed_attr_key = "shelby-turbo-tag"
+    @embed_attr_val = params[:shelbyTurboTag]
+
+    # Render embellishments if we find the correct video in the roll
+    @roll['frames'].each do |frame|
+      @video = frame['video']
+      if @video and @video['provider_name'] == params[:providerName] and @video['provider_id'] == params[:videoId]
+        render and return
+      end
+    end
+    
+    # fell through
+    render :nothing => true
   end
 
 
