2614425a193a931bcd12b3cf5213b1d4ca73b749
Dan Spinosa <spinosa@gmail.com>
2013-02-01 13:57:48 -0500

    simple JST integrated for page modification

--- app/assets/javascripts/turbo.js
+++ app/assets/javascripts/turbo.js
@@ -1,3 +1,15 @@
 //This is loaded by our small embed snippet on a host page.
 //That snippet looks something like the contents of ./turbo-embed/turbo-bootstrap.html.erb
+
+//Support
+//= require ./backbone/underscore.js
+
+// App
+//= require ./app/app.config.js
+//= require ./app/app.namespace.js
+
+// Turbo Templates
+//= require ../templates/turbo/embed_footer.js
+
+// Turbo Workhourse
 //= require ./turbo-embed/turbo.js.erb
