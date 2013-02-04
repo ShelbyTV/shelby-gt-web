93a94b2fd6b9e09d75f168d71974bf9b5e4814a0
Henry <henry@sztul.com>
2013-02-04 15:13:54 -0500

    entering youtube url handled

--- app/assets/javascripts/experience.js
+++ app/assets/javascripts/experience.js
@@ -5,7 +5,10 @@ $(document).ready(function(){
   //$('iframe.shelby').attr('src',"<%= Settings::Application.url %>/search");
 
   $('input:text').select();
-
+  $('input:text').focusin(function(){
+    $(this).attr('placeholder','').css('background-color','white');
+    console.log("selected");
+  });
 
   $('#learn-more-wrapper #learn-more-icon').hover(function(){
     $("#learn-more").toggle();
