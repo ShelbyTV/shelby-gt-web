f913458e88bb3e5b24bf7e9a60ef7b66e445a285
Michael Matyus <michael@maty.us>
2013-02-04 15:24:45 -0500

    small chat ui upgrades

--- app/assets/javascripts/app/views/discussion_roll/recipients.view.js
+++ app/assets/javascripts/app/views/discussion_roll/recipients.view.js
@@ -16,49 +16,49 @@ libs.shelbyGT.DiscussionRollRecipientsView = Support.CompositeView.extend({
   events: {
     "click .js-discussion-roll-recipients-toggle-show-all"  : "_toggleShowAllRecipients"
   },
-  
+
   initialize : function(){
     this.model.on('change:discussion_roll_participants', this.render, this);
     this.model.on('change:content_updated_at', this.render, this);
     this.render();
   },
-  
+
   _cleanup : function(){
     this.model.off('change:discussion_roll_participants', this.render);
     this.model.off('change:content_updated_at', this.render);
   },
-  
+
   template : function(obj){
     return SHELBYJST['discussion-roll/recipients'](obj);
   },
-  
+
   render : function(){
     var self = this;
-    
+
     if(this.options.updatePageTitle){
       //page title built iteratively by this and subviews
       document.title = "Shelby Mail with ";
     }
-    
+
     this.$el.html(this.template({discussionRoll: this.model}));
-    
+
     if(this.model.get('discussion_roll_participants')){
       var i = 0;
-      
-      this.model.get('discussion_roll_participants').forEach(function(p){ 
+
+      this.model.get('discussion_roll_participants').forEach(function(p){
         //show who this is TO (don't include the current viewer in that list unless they're the only participant)
         if( self.options.viewer !== p || self.model.get('discussion_roll_participants').length == 1 ){
           self.appendChildInto(
             new libs.shelbyGT.DiscussionRollRecipientView({
-                  model:p, 
-                  discussionRoll:self.model, 
-                  overflow:(i>=self.options.overflowAt), 
+                  model:p,
+                  discussionRoll:self.model,
+                  overflow:(i>=self.options.overflowAt),
                   updatePageTitle:self.options.updatePageTitle}),
             self.$('.js-discussion-roll-recipients-list') );
           i++;
         }
       });
-      
+
       if(i>this.options.overflowAt){
         //gt but ne b/c we skip viewer (above)
         this.$('.js-discussion-roll-recipients-toggle-show-all').show();
@@ -66,11 +66,11 @@ libs.shelbyGT.DiscussionRollRecipientsView = Support.CompositeView.extend({
     }
 
   },
-  
+
   _toggleShowAllRecipients: function(e){
     e.preventDefault();
-    
+    console.log(this.$el);
     this.$el.toggleClass("show-all-recipients");
   }
-  
+
 });
