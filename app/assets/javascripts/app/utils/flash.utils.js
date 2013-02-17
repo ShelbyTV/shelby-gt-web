a53f228e90096e099174291d1ab2ef9a8df49013
Michael Matyus <michael@maty.us>
2013-02-17 10:41:08 -0500

    no flash ironed out. also, accounting for lame h264 supportless firefox

--- app/assets/javascripts/app/utils/flash.utils.js
+++ app/assets/javascripts/app/utils/flash.utils.js
@@ -1,48 +1,41 @@
 libs.utils.flash = {
-  flashVersion : function() {
-    console.log('browser flash version');
-    return swfobject.getFlashPlayerVersion()
-  },
+  flashVersion : swfobject.getFlashPlayerVersion(),
 
   browserHasFlash : function() {
-    console.log('browser has flash, and browser is not an ipad');
-    return (this.flashVersion.major > 0) && !Browser.isIpad()
+    return this.flashVersion.major > 0;
   },
 
   flashDeclined : function() {
-    console.log('user declined notification');
-    return cookies.get('flash_declined') != "true"
+    return cookies.get('flash_declined') == "true";
   },
 
   h264Support : function() {
-    console.log('browser does not support h264');
-    return !Browser.isFirefox() || !Browser.isOpera();
+    return !Browser.isFirefox();
   },
 
-  html5no264Support : function() {
-    console.log('browser does not have flash, browser does not support h264')
+  noFlashno264Support : function() {
     return !this.h264Support() && !this.browserHasFlash();
   },
 
   detectFlash : function() {
-    console.log('detect flash');
-    if ( !this.browserHasFlash() && !this.flashDeclined() ) {
-    console.log('browser does not have flash and user has not declined notification');
-      shelby.dialog({
-        message: SHELBYJST['alert_no-flash'](),
-        button_primary : {
-          title: 'Install Flash Now'
-        },
-        button_secondary : {
-          title: 'No thanks'
-        }
-      },function(returnVal){
-        if(returnVal == libs.shelbyGT.notificationStateModel.ReturnValueButtonPrimary){
-          window.open('http://get.adobe.com/flashplayer/','_blank');
-        } else {
-          cookies.set('flash_declined','true',90);
-        }
-      });
+    if(!this.browserHasFlash() && !Browser.isIpad()){
+      if(!this.flashDeclined()) {
+        shelby.dialog({
+          message: SHELBYJST['alert_no-flash'](),
+          button_primary : {
+            title: 'Install Flash Now'
+          },
+          button_secondary : {
+            title: 'No thanks'
+          }
+        },function(returnVal){
+          if(returnVal == libs.shelbyGT.notificationStateModel.ReturnValueButtonPrimary){
+            window.open('http://get.adobe.com/flashplayer/','_blank');
+          } else {
+            cookies.set('flash_declined','true',90);
+          }
+        });
+      }
     }
   }
 };
