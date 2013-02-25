ac769ecc9f4622e79f41d741abb4bfc7fa07c089
Joshua Samberg <josh@shelby.tv>
2013-02-25 14:02:22 -0500

    On user profile info, dynamically render network name and currently playing roll name from corresponding data

--- app/assets/javascripts/app/views/user_profile_info.view.js
+++ app/assets/javascripts/app/views/user_profile_info.view.js
@@ -9,19 +9,24 @@ libs.shelbyGT.UserProfileInfoView = Support.CompositeView.extend({
     if (this.model.has('currentUser')) {
       this.model.get('currentUser').bind('change', this.render, this);
     }
+    this.options.guideModel.bind('change:activeFrameModel', this._onActiveFrameModelChange, this);
   },
 
   _cleanup : function(){
-   this.model.unbind('change:currentUser', this._onCurrentUserChange, this);
-   if (this.model.has('currentUser')) {
+    this.model.unbind('change:currentUser', this._onCurrentUserChange, this);
+    if (this.model.has('currentUser')) {
       this.model.get('currentUser').unbind('change', this.render, this);
     }
+    this.options.guideModel.unbind('change:activeFrameModel', this._onActiveFrameModelChange, this);
   },
 
   render : function(){
     var currentUser = this.model.get('currentUser');
     this._leaveChildren();
-    this.$el.html(this.template({user: currentUser}));
+    this.$el.html(this.template({
+      user : currentUser,
+      frame : this.options.guideModel.get('activeFrameModel')
+    }));
     if (currentUser && !currentUser.isNew()) {
       this.appendChild(new libs.shelbyGT.PersistentVideoInfoView({
         className : 'animate_module media_module js-inactivity-preemption user_profile_persistent_video_info__wrapper',
@@ -44,6 +49,11 @@ libs.shelbyGT.UserProfileInfoView = Support.CompositeView.extend({
     if (previousUser) {
       previousUser.unbind('change', this.render, this);
     }
+  },
+
+  _onActiveFrameModelChange : function(guideModel, activeFrameModel) {
+    var rollTitle = activeFrameModel && activeFrameModel.has('roll') && activeFrameModel.get('roll').get('title');
+    this.$('.js-youre-watching').text("You're watching: " + (rollTitle ? rollTitle : 'shelby.tv'));
   }
 
 });
