bedf54a8a00388047ed71a697f1f03563454c066
Joshua Samberg <josh@shelby.tv>
2013-02-26 11:59:37 -0500

    Enabled paging in roll views on new .tv user profile

--- app/assets/javascripts/app/views/user_channel_item.view.js
+++ app/assets/javascripts/app/views/user_channel_item.view.js
@@ -2,14 +2,19 @@ libs.shelbyGT.UserChannelItemView = libs.shelbyGT.ActiveHighlightListItemView.ex
 
   _channelListView : null,
 
-  events : {
-  },
-
   options : _.extend({}, libs.shelbyGT.ActiveHighlightListItemView.prototype.options, {
       activationStateProperty : 'activeFrameModel'
   }),
 
   className : 'list_item user-channel-item',
+
+  initialize : function() {
+    this.model.bind(libs.shelbyGT.ShelbyBaseModel.prototype.messages.fetchComplete, this._onFetchComplete, this);
+  },
+
+  _cleanup : function() {
+    this.model.unbind(libs.shelbyGT.ShelbyBaseModel.prototype.messages.fetchComplete, this._onFetchComplete, this);
+  },
 
   template : function(obj){
     return SHELBYJST['user-channel-item'](obj);
@@ -25,21 +30,21 @@ libs.shelbyGT.UserChannelItemView = libs.shelbyGT.ActiveHighlightListItemView.ex
         include_children : true
       },
       noMoreResultsViewProto : null,
-      firstFetchLimit : 5,
-      infinite : false,
+      firstFetchLimit : shelby.config.pageLoadSizes.rollOnUserProfile.initialLoad,
       isIntervalComplete : function() {
         return false;
       },
-      limit : 6,
+      limit : shelby.config.pageLoadSizes.rollOnUserProfile.pagingLoad,
       listItemView : 'UserChannelFrameItemView',
       model : this.model
     });
     this.appendChildInto(this._channelListView, '.js-user-channel');
+    this._sizeToContents();
     this.model.fetch({
-      success: function(rollModel, resp){
-        var newWidth = self._channelListView._displayCollection.length * 300 + 150;
-        self.$('.js-user-channel').width(newWidth + 'px');
-
+      data : {
+        limit : shelby.config.pageLoadSizes.rollOnUserProfile.initialLoad
+      },
+      success : function(rollModel, resp){
         // if this is the current user's personal roll and nothing is playing,
         // register this roll as the current playlist and start playing the first frame in the roll
         var currentUserPublicRollId = self.options.userProfileModel.get('currentUser') &&
@@ -57,6 +62,19 @@ libs.shelbyGT.UserChannelItemView = libs.shelbyGT.ActiveHighlightListItemView.ex
   doActivateThisItem : function(guideModel){
     var activeFrameModel = guideModel.get('activeFrameModel');
     return activeFrameModel && activeFrameModel.has('roll') && this.model.id == activeFrameModel.get('roll').id;
+  },
+
+  _onFetchComplete : function(rollModel, resp){
+    this._sizeToContents();
+  },
+
+  // _sizeToContents - since this is a horizontal scrolling container, we need to explicitly set its width
+  // to match the combined width of its contents when that quantity changes
+  _sizeToContents : function(){
+    var $userChannels = this.$('.js-user-channel-item');
+    var newWidth = ($userChannels.length * $userChannels.outerWidth(true)) +
+                this.$('.js-user-channel-title').outerWidth(true) + this.$('.js-load-more').outerWidth(true);
+    this.$('.js-user-channel').width(newWidth);
   }
 
 });
