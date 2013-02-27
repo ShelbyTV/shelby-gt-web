ed16ba6f2b36d90991f86a8263622570327b6c79
Dan Spinosa <spinosa@gmail.com>
2013-02-27 12:08:29 -0500

    hotfix: meta tag updates for SEO

--- app/controllers/roll_controller.rb
+++ app/controllers/roll_controller.rb
@@ -18,7 +18,7 @@ class RollController < ApplicationController
   def show_personal_roll
     user_id = params[:user_id]
     @user = Shelby::API.get_user(user_id) if user_id
-    @roll = Shelby::API.get_roll(@user['personal_roll_id']) if @user
+    @roll = Shelby::API.get_roll_with_frames(@user['personal_roll_id']) if @user
     render '/home/app'
   end
 
@@ -44,7 +44,7 @@ class RollController < ApplicationController
 
     def get_roll_and_roll_creator_by_roll_id
       roll_id = params[:roll_id]
-      @roll = BSON::ObjectId.legal?(roll_id) ? Shelby::API.get_roll(roll_id) : nil
+      @roll = BSON::ObjectId.legal?(roll_id) ? Shelby::API.get_roll_with_frames(roll_id) : nil
       @user = Shelby::API.get_user(@roll['creator_id']) if @roll
     end
 
