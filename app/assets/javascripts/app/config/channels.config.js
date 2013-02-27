b83333fe8f05822c04939c0f78eb1c0aa0a0935b
Henry <henry@sztul.com>
2013-02-27 10:24:09 -0500

    updating keys in channel config

--- app/assets/javascripts/app/config/channels.config.js
+++ app/assets/javascripts/app/config/channels.config.js
@@ -1,17 +1,17 @@
 shelby.config.channels = {
-  '1' : {
+  'fun' : {
     title : '#fun',
     id : '5112fa93b415cc1de20c30a0'
   },
-  '2' : {
+  'happenings' : {
     title : '#happenings',
     id : '5112fb26b415cc1e160cb0e5'
   },
-  '3' : {
+  'learning' : {
     title : '#learning',
     id : '5112fb5db415cc1ded0d79a1'
   },
-  '4' : {
+  'adrenaline' : {
     title : '#adrenaline',
     id : '5112fb95b415cc1ded0d8c89'
   }
