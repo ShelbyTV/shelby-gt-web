41f77abb06f22243761def048b45db10e499c372
Henry <henry@sztul.com>
2013-02-27 09:56:32 -0500

    change channel title names

--- app/assets/javascripts/app/config/channels.config.js
+++ app/assets/javascripts/app/config/channels.config.js
@@ -1,18 +1,18 @@
 shelby.config.channels = {
   '1' : {
-    title : 'Fun',
+    title : '#fun',
     id : '5112fa93b415cc1de20c30a0'
   },
   '2' : {
-    title : 'News',
+    title : '#happenings',
     id : '5112fb26b415cc1e160cb0e5'
   },
   '3' : {
-    title : 'Edu',
+    title : '#learning',
     id : '5112fb5db415cc1ded0d79a1'
   },
   '4' : {
-    title : 'Nature',
+    title : '#adrenaline',
     id : '5112fb95b415cc1ded0d8c89'
   }
 };
