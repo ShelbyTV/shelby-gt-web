660208c6b5bab190cd71dcf14c95ada1805e9057
Dan Spinosa <spinosa@gmail.com>
2013-02-28 19:53:04 -0500

    don't render the second channel selector (that overlays the video)

--- app/assets/javascripts/app/views/video_content_pane.view.js
+++ app/assets/javascripts/app/views/video_content_pane.view.js
@@ -38,11 +38,11 @@ libs.shelbyGT.VideoContentPaneView = Support.CompositeView.extend({
       model : shelby.models.notificationState
     }));
 
-    this.renderChild(new libs.shelbyGT.ChannelInfoOverlayView({
+    /*this.renderChild(new libs.shelbyGT.ChannelInfoOverlayView({
       el: this.$('#js-channel-info-overlay-wrapper'),
       model : shelby.models.guide,
       playlistManagerModel : shelby.models.playlistManager
-    }));
+    }));*/
     this.renderChild(new libs.shelbyGT.VideoDisplayView({
       model : shelby.models.guide,
       playbackState : shelby.models.playbackState,
