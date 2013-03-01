d064ccf87f7064449ebda745feeff4fe55d1188d
Joshua Samberg <josh@shelby.tv>
2013-03-01 09:58:58 -0500

    Enable the A/B test for new vs old .tv look

--- app/assets/javascripts/app/routers/dynamic.router.js
+++ app/assets/javascripts/app/routers/dynamic.router.js
@@ -125,9 +125,7 @@ libs.shelbyGT.DynamicRouter = Backbone.Router.extend({
   },
 
   displayIsolatedRoll : function(rollId, params){
-    // TODO: turn this back on before production deploy
-    // if (_(shelby.config.dotTvNetworks.userProfileViewRollIds).contains(rollId)) {
-    if (!_(shelby.config.dotTvNetworks.oldDotTvRollIds).contains(rollId)) {
+    if (shelby.abTests.dotTvLayout == 'user_profile' && _(shelby.config.dotTvNetworks.userProfileViewRollIds).contains(rollId)) {
       this._setupUserProfileView({
         rollId : rollId,
         params : params
