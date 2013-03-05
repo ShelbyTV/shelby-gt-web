01a6c4b4effe434338e34802006ad944fae390fa
Henry <henry@sztul.com>
2013-03-05 17:11:51 -0500

    change to config with new names

--- app/assets/javascripts/app/config/channels.config.js
+++ app/assets/javascripts/app/config/channels.config.js
@@ -1,14 +1,14 @@
 shelby.config.channels = {
-  'fun' : {
-    title : '#fun',
+  'laugh' : {
+    title : '#laugh',
     id : '5112fa93b415cc1de20c30a0'
   },
   'happenings' : {
     title : '#happenings',
     id : '5112fb26b415cc1e160cb0e5'
   },
-  'learning' : {
-    title : '#learning',
+  'learn' : {
+    title : '#learnaboutyourworld',
     id : '5112fb5db415cc1ded0d79a1'
   },
   'adrenaline' : {
