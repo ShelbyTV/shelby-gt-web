b983194c765d7f0a9d6b928d353e4c254ff2c8e2
Henry <henry@sztul.com>
2013-03-14 16:51:17 -0400

    adding custom data to intercom

--- app/assets/javascripts/app/utils/intercom.utils.js
+++ app/assets/javascripts/app/utils/intercom.utils.js
@@ -8,7 +8,9 @@ libs.utils.intercom = {
               email: userModel.get('primary_email'),
               created_at: created_at,
               name: userModel.get('name'),
-              user_id: userModel.get('id')
+              user_id: userModel.get('id'),
+              tv_page: "http://"+userModel.get('personal_roll_subdomain')+'.shelby.tv',
+              preferences_email_updates: userModel.get('preferences')['email_updates']
             });
     } catch (e) {console.log("[ INTERCOM ERROR ] ", e);}
 
