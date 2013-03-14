3510ae31f4f57edcec83d295414c8267a53052b9
Henry <henry@sztul.com>
2013-03-14 16:45:16 -0400

    updating unix timestamp for intercom

--- app/assets/javascripts/app/utils/intercom.utils.js
+++ app/assets/javascripts/app/utils/intercom.utils.js
@@ -2,7 +2,7 @@ libs.utils.intercom = {
   boot : function(userModel){
     try {
       created_at = new Date(userModel.get('created_at').slice(0,10));
-      created_at = created_at.getTime();
+      created_at = created_at.getTime()/1000;
       Intercom('boot', {
               app_id: 'aeb096feb787399ac1cf3985f891d0e13aa47571',
               email: userModel.get('primary_email'),
