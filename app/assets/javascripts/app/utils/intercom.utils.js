b0a3152fb2d792d2d1a0eb090520f57f7de46c25
Dan Spinosa <spinosa@gmail.com>
2013-03-15 09:35:29 -0400

    don't try to boot Intercom when it's not defined

--- app/assets/javascripts/app/utils/intercom.utils.js
+++ app/assets/javascripts/app/utils/intercom.utils.js
@@ -1,16 +1,17 @@
 libs.utils.intercom = {
   boot : function(userModel){
-    try {
-      created_at = new Date(userModel.get('created_at').slice(0,10));
-      created_at = created_at.getTime();
-      Intercom('boot', {
-              app_id: 'aeb096feb787399ac1cf3985f891d0e13aa47571',
-              email: userModel.get('primary_email'),
-              created_at: created_at,
-              name: userModel.get('name'),
-              user_id: userModel.get('id')
-            });
-    } catch (e) {console.log("[ INTERCOM ERROR ] ", e);}
-
+    if(typeof(Intercom) !== "undefined"){
+      try {
+        created_at = new Date(userModel.get('created_at').slice(0,10));
+        created_at = created_at.getTime();
+        Intercom('boot', {
+                app_id: 'aeb096feb787399ac1cf3985f891d0e13aa47571',
+                email: userModel.get('primary_email'),
+                created_at: created_at,
+                name: userModel.get('name'),
+                user_id: userModel.get('id')
+              });
+      } catch (e) {console.log("[ INTERCOM ERROR ] ", e);}
+    }
   }
 };
