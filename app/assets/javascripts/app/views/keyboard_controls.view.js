af0716276da318c227d7fefaac1db2a2e97c7743
Henry <henry@sztul.com>
2013-04-02 10:41:39 -0400

    updated keyboard shortcut and hiding tooltips on channel welcome unless ? is pressed

--- app/assets/javascripts/app/views/keyboard_controls.view.js
+++ app/assets/javascripts/app/views/keyboard_controls.view.js
@@ -150,7 +150,7 @@
         }
       },
       // (s)hort link
-      76 : {
+      83 : {
         model : 'userDesires',
         attr : 'shortLink',
         val : function(){
@@ -185,8 +185,10 @@
             if ($('#js-welcome').hasClass('hidden')){
               shelby.userInactivity.disableUserActivityDetection();
               shelby.models.userDesires.set({guideShown: false});
+              $('.tooltip').show();
             }
             else {
+              $('.tooltip').hide();
               shelby.userInactivity.enableUserActivityDetection();
             }
             $('#js-welcome, .js-channels-welcome').toggleClass('hidden', !$('#js-welcome').hasClass('hidden'));
@@ -252,7 +254,7 @@
       });
       $(document).on('keydown', function(e){
         if(shelby.models.userDesires.get('typing')) return true;
-        var actionData = self._getActionData(e.keyCode);
+        var actionData = self._keyCodeActionMap[e.keyCode];
         if(!actionData) return true;
         if (_(actionData).has('preventDefault') && _(actionData.preventDefault).has('keyDown')) {
           if (_(actionData.preventDefault).result('keyDown')) {
