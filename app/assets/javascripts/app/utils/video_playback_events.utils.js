a06c00e13de3d8d53b80691ec344ff87e9882596
Dan Spinosa <spinosa@gmail.com>
2013-04-12 11:38:07 -0400

    fix typo in playback event logic, increase 'watched interval' reporting from 5 to 15s in same logic

--- app/assets/javascripts/app/utils/video_playback_events.utils.js
+++ app/assets/javascripts/app/utils/video_playback_events.utils.js
@@ -13,7 +13,7 @@
     _userSeeked : false,
 
     //how much playback (in seconds) should eached "watched" interval represent
-    WATCHED_INTERVAL : 5,
+    WATCHED_INTERVAL : 15,
     //how much playback (by percent) should eached "watched" interval represent
     WATCHED_PCT : 0.20,
     //when to mark video as watched (by percent) for js based even tracking
@@ -66,6 +66,7 @@
       // did they watch the mininum amount in seconds or by percentage?
       var watchedSeconds = curTime - this._startTime;
       if( watchedSeconds > this.WATCHED_INTERVAL || watchedSeconds > this._requiredWatchPct) {
+        //API intelligently handles multiple "watched" posts for same frame
         this._currentFrame.watched(this._startTime, curTime);
         this._startTime = curTime;
 
@@ -123,7 +124,7 @@
     _videoChanged: function( guideModel, frame ){
       this._currentFrame = frame;
       this._startTime = 0;
-      this._requireWatchPct = frame.get('video').get('duration') ? (frame.get('video').get('duration') * this.WATCHED_PCT) : this.WATCHED_INTERVAL;
+      this._requiredWatchPct = frame.get('video').get('duration') ? (frame.get('video').get('duration') * this.WATCHED_PCT) : this.WATCHED_INTERVAL;
       this._markedAsWatched = null;
     },
 
