930a81c803c5334fa7786f1b7f148c856bffa4fd
Henry <henry@sztul.com>
2013-04-12 20:02:52 -0400

    styled. can now show

--- app/assets/javascripts/app/views/email_collection.view.js
+++ app/assets/javascripts/app/views/email_collection.view.js
@@ -1,7 +1,7 @@
 libs.shelbyGT.emailCollection = Support.CompositeView.extend({
 
   events : {
-
+      "click .js-close" : "_close"
   },
 
   template : function(obj){
@@ -9,10 +9,12 @@ libs.shelbyGT.emailCollection = Support.CompositeView.extend({
   },
 
   initialize : function(){
+    Backbone.Events.bind('show:emailCollection', this._onShowEmailCollection, this);
     this.render();
   },
 
   cleanup : function(){
+    Backbone.Events.unbind('show:emailCollection', this._onShowEmailCollection, this);
   },
 
   render : function(){
@@ -26,7 +28,14 @@ libs.shelbyGT.emailCollection = Support.CompositeView.extend({
         gaLabel : 'Channels',
         kmName : "Email collection modal loaded on channels"
       });
-  }
+  },
 
+  _onShowEmailCollection : function(){
+    this.$el.show();
+  },
+
+  _close : function() {
+    this.$el.toggleClass('hidden');
+  }
 
 });
