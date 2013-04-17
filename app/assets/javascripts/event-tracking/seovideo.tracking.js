5a2232be9ee609e1485f7eaa1008aa5120d974e3
Henry <henry@sztul.com>
2013-04-17 10:31:23 -0400

    [hotfix] adding click tracking to seo pages

--- app/assets/javascripts/event-tracking/seovideo.tracking.js
+++ app/assets/javascripts/event-tracking/seovideo.tracking.js
@@ -5,24 +5,14 @@
 // add class 'js-track-event' to element with data attributes for a category, action and label
 //---------------------------------------------------------
 
-// 02/08/13 (HIS)
-// TURNING OFF ALL TRACKING ON SEO FOR THE TIME BEING TO REDUCE # EVENTS SENT TO KISS
-
-/*$(document).ready(function(){
-	$('.js-track-event').on('click', function(e){
-
+$(document).ready(function(){
+  $('.js-track-event').on('click', function(e){
     var action = $(e.currentTarget).data("ga_action");
     var category = $(e.currentTarget).data("ga_category");
     var label = $(e.currentTarget).data("ga_label");
-
     try {
-			_gaq.push(['_trackEvent', category, action, label]);
-
-      var kmq_action = action + ' on ' + category;
-
-      _kmq.push(['record', kmq_action]);
+      _gaq.push(['_trackEvent', category, action, label]);
     }
-
-    catch(e){};
-	});
-});*/
+    catch(e) {}
+  });
+});
