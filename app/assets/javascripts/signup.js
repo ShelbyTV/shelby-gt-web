f44dedc6e3d8e86658774eef1fa848d29a3f80eb
Michael Matyus <michael@maty.us>
2013-05-21 12:22:03 -0400

    slideshow hashchange mgmt

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -3,18 +3,40 @@
 
 //= require ./jquery-plugins/jquery.cycle.js
 
-(function(){
-  //cool!
+var onBefore = function(foo,bar,baz){
+  // console.log('onBefore',this);
+  var $index = $(this).data('index');
+  $('.js-next').attr('disabled',($index >= 4));
+};
 
-  var $slideshow = $('.js-slideshow');
+var onAfter = function(foo,bar,baz){
+  // console.log('onAfter',this);
+  var $index = $(this).data('index');
+
+  window.location.hash = ($index);
+
+  // console.log($index, ($index >= 4));
+};
+
+(function(){
+
+  $slideshow = $('.js-slideshow');
+
+  var startingSlide = function() {
+    var num = +(window.location.hash.split('#')[1]);
+    return (!isNaN(num) && num >= 0) ? num : 0;
+  };
 
   $slideshow.cycle({
     fx:   'scrollLeft',
     next: '.js-advance-slideshow',
-    timeout: 0
+    timeout: 0,
+    before: onBefore,
+    after: onAfter,
+    startingSlide: startingSlide()
   });
 
-  var $followUnfollow = $('.js-followOrUnfollow');
+  var $followUnfollow = $('.js-followOrUnfollow').data('follow',false);
 
   $followUnfollow.on('click',function(e){
     e.preventDefault();
@@ -24,6 +46,11 @@
     $this.toggleClass('button_gray',!$this.hasClass('button_gray'));
     $this.toggleClass('button_green',!$this.hasClass('button_gray'));
 
+  });
+
+  $(window).on('hashchange',function(e){
+    e.preventDefault();
+    $slideshow.cycle(+(window.location.hash.split('#')[1]));
   });
 
 })();
