07a300b4e3db51e5caa6a13877a6add3a00ad54c
Michael Matyus <michael@maty.us>
2013-05-20 20:00:37 -0400

    trying to catch up on the UI changes and stuff.

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -1,6 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+//= require ./jquery-plugins/jquery.cycle.js
+
 (function(){
   //cool!
+
+  // var $slideshow = $('.js-slideshow');
+
+  // $slideshow.cycle({
+  //   fx:   'scrollLeft',
+  //   next: '.js-advance-slideshow'
+  // });
+
 })();
