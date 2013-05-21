fe0068e7105b9f0a9a8a70431a913d26edd238e5
Michael Matyus <michael@maty.us>
2013-05-20 20:13:14 -0400

    slideshow of stuff. need actual data solution

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -6,11 +6,23 @@
 (function(){
   //cool!
 
-  // var $slideshow = $('.js-slideshow');
+  var $slideshow = $('.js-slideshow');
 
-  // $slideshow.cycle({
-  //   fx:   'scrollLeft',
-  //   next: '.js-advance-slideshow'
-  // });
+  $slideshow.cycle({
+    fx:   'scrollLeft',
+    next: '.js-advance-slideshow',
+    timeout: 0
+  });
+
+  var $followUnfollow = $('.js-followOrUnfollow');
+
+  $followUnfollow.on('click',function(e){
+    e.preventDefault();
+
+    var $this = $(this);
+
+    $this.toggleClass('button_gray',!$this.hasClass('button_gray'));
+    $this.toggleClass('button_green',!$this.hasClass('button_gray'));
+  });
 
 })();
