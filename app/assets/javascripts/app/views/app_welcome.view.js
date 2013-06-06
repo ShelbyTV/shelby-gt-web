56be3ee8d7177418eeaf50613c6c7ecf16170b86
Henry <henry@sztul.com>
2013-06-06 16:40:30 -0400

    showing an app welcome banner

---
+++ app/assets/javascripts/app/views/app_welcome.view.js
@@ -0,0 +1,34 @@
+libs.shelbyGT.appWelcome = Support.CompositeView.extend({
+
+  events : {
+    "click .js-play"        :       "_onClickPlay"
+  },
+
+  template : function(obj){
+    return SHELBYJST['app-welcome'](obj);
+  },
+
+  initialize : function(){
+    if (!shelby.models.user.get('app_progress').hasBeenWelcomed()){
+      shelby.models.playbackState.set('autoplayOnVideoDisplay', false);
+      shelby.userInactivity.disableUserActivityDetection();
+      this.render();
+    }
+  },
+
+  cleanup : function(){
+  },
+
+  render : function(){
+    this.$el.html(this.template());
+    $('#js-welcome, .js-app-welcome').removeClass('hidden');
+  },
+
+  _onClickPlay : function() {
+      shelby.models.playbackState.set('autoplayOnVideoDisplay', true);
+      shelby.userInactivity.enableUserActivityDetection();
+      $('#js-welcome, .js-app-welcome').addClass('hidden');
+      //shelby.models.userDesires.set('playbackStatus',libs.shelbyGT.PlaybackStatus.playing);
+  }
+
+});
