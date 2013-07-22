e25bb0b001b1f131311207e3c0f5511d8d518050
Michael Matyus <michael@maty.us>
2013-07-22 15:38:59 -0400

    progress on fb post and share URL

--- app/assets/javascripts/app/views/user_preferences_friends.view.js
+++ app/assets/javascripts/app/views/user_preferences_friends.view.js
@@ -1,6 +1,9 @@
 libs.shelbyGT.UserPreferencesFriendsView = libs.shelbyGT.UserPreferencesBaseView.extend({
 
+  _baseURL : 'http://shelby.tv/invite?id=',
+
   events : {
+    'click .js-facebook-post' : '_shareToFacebook'
   },
 
   className: 'content_lining preferences_page preferences_page--friends',
@@ -15,6 +18,7 @@ libs.shelbyGT.UserPreferencesFriendsView = libs.shelbyGT.UserPreferencesBaseView
     var userId = shelby.models.user.get('id');
 
     var data = {
+      shareURL : this._buildShareUrl(userId),
 			tweetURL : this._buildTweetUrl(userId)
 		};
 
@@ -31,11 +35,41 @@ libs.shelbyGT.UserPreferencesFriendsView = libs.shelbyGT.UserPreferencesBaseView
   _buildTweetUrl : function(userId) {
 
     var tweetText = encodeURIComponent('Friends, join me on Shelby'),
-        tweetUrl  = 'http://shelby.tv/invite?id=' + userId;
+        tweetUrl  = this._baseURL + userId;
 
     var url = 'https://twitter.com/intent/tweet?related=shelby&via=shelby&url=' + tweetUrl + '&text=' + tweetText + '';
 
     return url;
+  },
+
+  _buildShareUrl : function(userId) {
+    return this._baseURL + userId;
+  },
+
+  _shareToFacebook : function(e){
+    e.preventDefault();
+
+    if($(e.currentTarget).hasClass('disabled')){
+      return;
+    }
+
+    if (typeof FB != "undefined"){
+      FB.ui(
+        {
+          caption     : 'text',
+          description : 'videoDescription',
+          link        : 'http://shelby.tv/invite',
+          method      : 'feed',
+          name        : 'videoTitle',
+          picture     : 'videoThumbnai'
+        },
+        function(response) {
+          if (response && response.post_id) {
+            // TODO:we should record that this happened.
+          }
+        }
+      );
+    }
   }
 
 });
