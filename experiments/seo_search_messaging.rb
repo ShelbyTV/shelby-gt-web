97f79fb1ec84477db924338ea8f4cec07c696225
Henry <henry@sztul.com>
2013-08-02 11:45:38 -0400

    adding ab test for seo page messaging

--- experiments/seo_search_messaging.rb
+++
@@ -1,5 +0,0 @@
-ab_test "seo_search_messaging" do
-  description "Try different messaging/calls to action to get more search going on"
-  alternatives :sell_providers, :sell_video
-  metrics :tracked_externally
-end
