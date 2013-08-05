a61146381029cf7942c861785e6a45cc7dcc90ab
Henry <henry@sztul.com>
2013-08-05 09:57:32 -0400

    updating landing and seo tests. making them ab rather than abcd

--- experiments/landing_messaging.rb
+++ experiments/landing_messaging.rb
@@ -1,5 +1,5 @@
 ab_test "landing_messaging" do
   description "Try different messaging to increase click through to signup"
-  alternatives :videos_youll_love, :discover_and_enjoy, :just_for_you, :powered_by_friends
+  alternatives :discover_and_enjoy, :powered_by_friends
   metrics :tracked_externally
 end
--- experiments/seo_messaging.rb
+++ experiments/seo_messaging.rb
@@ -1,5 +1,5 @@
 ab_test "seo_messaging" do
   description "Try different messaging/calls to action to get more clicking going on"
-  alternatives :videos_youll_love, :discover_and_enjoy, :just_for_you, :no_search_required
+  alternatives :discover_and_enjoy, :powered_by_friends
   metrics :tracked_externally
 end
