553b893dcabf9d75c52475b8151bf1e6aeb6c4ca
Michael Matyus <michael@maty.us>
2013-08-20 14:40:19 -0400

    pretty hacky, but whatever

--- app/assets/javascripts/shares.js
+++ app/assets/javascripts/shares.js
@@ -7,4 +7,26 @@ $(document).ready(function(){
     var $this = $(this);
     $this.toggleClass('line-clamp--open', !$this.hasClass('line-clamp--open'));
   });
+
+
+  $('.js-content-selector').on('click','button',function(e){
+    e.preventDefault();
+    var $this = $(this);
+
+    if($this.hasClass('js-stream')) {
+      window.location = "/stream";
+    }
+    else if($this.hasClass('js-community')) {
+      window.location = "/community";
+    }
+    else if($this.hasClass('js-me')) {
+      var username = $('.app_nav__button--settings').text().trim();
+      window.location = '/' + username;
+    }
+    else if($this.hasClass('js-signout')) {
+      window.location = "/signout";
+    } else {
+      window.location = $this.attr('href');
+    }
+  });
 });
