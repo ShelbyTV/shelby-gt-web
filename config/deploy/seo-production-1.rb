fd140fd9d4a430c9af860fd01bf0be4994baac5e
Henry <henry@sztul.com>
2013-09-06 16:17:07 -0400

    adding new environment

---
+++ config/deploy/seo-production-1.rb
@@ -0,0 +1,16 @@
+#############################################################
+# Servers
+#############################################################
+
+role :web, "119.9.14.236"
+role :app, "119.9.14.236"
+
+#############################################################
+# Git
+#############################################################
+
+set :repository,  "git@github.com:ShelbyTV/shelby-gt-web.git"
+set :branch, "onshelby-tv"
+set :rails_env, "seo-production-1"
+set :unicorn_env, "production"
+set :app_env,     "seo-production-1"
