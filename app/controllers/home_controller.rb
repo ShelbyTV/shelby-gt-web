606198f67beb3b7b094c848eaa3ca4f77a2df789
Henry <henry@sztul.com>
2013-09-08 14:10:07 -0400

    making app not accessible. cleaning links. updated robots text to point to new site index to be created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -24,66 +24,30 @@ class HomeController < ApplicationController
       # only meant to handle requests for html pages
       format.html {
 
-        #XXX .TV subdomains
-        # This is such a hack.  I'd like to detect this in routes.rb and handle by sending to another
-        # controller, but until that's built, we just short-circuit right here
-        if dot_tv_roll = get_dot_tv_roll_from_domain(request)
-          user = Shelby::API.get_user(dot_tv_roll['creator_id'])
-          frame_id = get_frame_from_path(params[:path])
 
-          unless frame_id
-            # if no frame was specified, this points to the user's roll, which is now at the user's "profile", shelby.tv/the-users-nickname
-            redirect_to "#{Settings::Application.protocol_matching_url}/#{user['nickname']}", :status => :moved_permanently and return
-          else
-            # if a frame was specified this is a link to a specific frame on the user's roll
-            redirect_to "#{Settings::Application.protocol_matching_url}/roll/#{dot_tv_roll['id']}/frame/#{frame_id}", :status => :moved_permanently and return
-          end
+        # Consider errors and render landing page
+        @mobile_os     = detect_mobile_os
+        @is_mobile     = is_mobile?
+
+
+        if flash[:user_errors]
+          @user_attributes = flash[:user_attributes]
+          @email_error = flash[:user_errors_email]
+          @nickname_error = flash[:user_errors_nickname]
         end
 
-        path = params[:path]
-        # if the path is just a user name, this is the route for a user profile, so get the
-        # necessary view instance variables and render
-        if path && !Settings::Application.root_paths.include?(path)
-          user = Shelby::API.get_user(path)
+        view_context.get_info_for_meta_tags(params[:path])
 
-          if user
-            @user = user
-            @roll = Shelby::API.get_roll_with_frames(@user['personal_roll_id']) if @user
-            render '/home/app' and return
-          end
-        end
+        # if @mobile_os
+        #   render '/mobile/search', :layout => 'mobile'
+        # else
 
-        if user_signed_in?
-          render '/home/app'
-        else
-          # Consider errors and render landing page
-          @auth_failure  = params[:auth_failure] == '1'
-          @auth_strategy = params[:auth_strategy]
-          @access_error  = params[:access] == "nos"
-          @invite_error  = params[:invite] == "invalid"
-          @mobile_os     = detect_mobile_os
-          @is_mobile     = is_mobile?
+        # A/B tests
+        #@landing_messaging_v2 = ab_test :landing_messaging_v2
+        #@signup_on_landing = ab_test :signup_on_landing
+        #@signup_w_fb = ab_test :signup_w_fb
 
-
-          if flash[:user_errors]
-            @user_attributes = flash[:user_attributes]
-            @email_error = flash[:user_errors_email]
-            @nickname_error = flash[:user_errors_nickname]
-          end
-
-          view_context.get_info_for_meta_tags(params[:path])
-
-          # if @mobile_os
-          #   render '/mobile/search', :layout => 'mobile'
-          # else
-
-          # A/B tests
-          #@landing_messaging_v2 = ab_test :landing_messaging_v2
-          #@signup_on_landing = ab_test :signup_on_landing
-          #@signup_w_fb = ab_test :signup_w_fb
-
-          render '/home/landing'
-        end
+        render '/home/landing'
 
       }
       # if we hit this as the catch-all while looking for an image or some other special format,
@@ -104,197 +68,8 @@ class HomeController < ApplicationController
     render '/home/landing'
   end
 
-  ##
-  # Handles community view when visited directly (allowing logged-out users to see it)
-  #
-  # GET /community
-  #
-  def community
-    render '/home/app'
-  end
-
-  ##
-  # Handles featured view when visited directly (allowing logged-out users to see it)
-  #
-  # GET /featured
-  #
-  def featured
-    render '/home/app'
-  end
-
-
-  ##
-  # Handles channels view when visited directly (allowing logged-out users to see it)
-  #
-  # GET /channels
-  #
-  def channels
-    render '/home/app'
-  end
-
-  ##
-  # Handles search view when visited directly (allowing logged-out users to see it)
-  #
-  # GET /search
-  #
-  def search
-    # TODO: uncomment when we have html and js to implement the test variants
-    # unless user_signed_in?
-    #   # A/B test - only for anonymous users
-    #   @search_landing_banner_appear = ab_test :search_landing_banner_appear
-    # end
-    render '/home/app'
-  end
-
-  ##
-  # Handles channel view when visited directly (allowing logged-out users to see it)
-  #
-  # GET /channel/:name
-  #
-  def channel
-    render '/home/app'
-  end
-
   # Static page of information
   def learn_more
   end
-
-  # Static page with stats on a users recent activity
-  def stats
-    # lookup user + stats via api
-    if user_signed_in? and request.headers['HTTP_COOKIE'] and @user = Shelby::API.get_user(params['user_id'])
-      @frames = Shelby::API.get_user_stats(params['user_id'], request.headers['HTTP_COOKIE'])
-    end
-  end
-
-  ##
-  # Handles shares view when visited directly (allowing logged-out users to see it)
-  #
-  # GET /:user_name/shares(/:frame_id)
-  #
-  def shares
-
-    user_id_or_nickname = params[:user_id_or_nickname]
-    user = Shelby::API.get_user(user_id_or_nickname)
-
-    @user = user if user
-    @user_signed_in = user_signed_in?
-    @current_user = Shelby::API.get_user(current_user_id) if @user_signed_in
-    @current_user_nickname = (@current_user['nickname'] if @current_user) || 'Anonymous'
-    @roll = Shelby::API.get_roll_with_frames(@user['personal_roll_id']) if @user
-
-    if(params[:frame_id])
-      @frame = (Shelby::API.get_frame(params[:frame_id], true))
-      @video = (@frame['video'] if @frame)
-
-      @is_mobile = is_mobile?
-
-      if request.env['HTTP_REFERER']
-        ref = request.env['HTTP_REFERER']
-        @has_referer = ref.match(/twitter|facebook|t.co/)
-      end
-
-      render '/home/shares'
-    else
-      render '/home/app'
-    end
-  end
-
-  ##
-  # Handles "make the web" (allowing logged-out users to see it)
-  #
-  # GET /experience/:url
-  #
-  def experience
-    urls = ["http://www.reddit.com/r/videos",
-            "http://www.reddit.com/domain/hulu.com",
-            "http://laughingsquid.com",
-            "http://vimeo.com/channels/documentaryfilm",
-            "http://vimeo.com/channels/7588",
-            "http://vimeo.com/channels/worldhd",
-            "http://mashable.com/2012/07/12/funny-youtube-videos-reddit/",
-            "http://youtube-global.blogspot.com/",
-            "http://periodicvideos.blogspot.com/",
-            "http://youtube-trends.blogspot.com/"
-          ]
-    @url = params[:q] ? params[:q] : urls[rand(urls.length)]
-    render '/home/experience'
-  end
-
-  ##
-  # GT API Server sets the appropriate cookie to let us know the user is signed out
-  #  in case something went wrong somewhere over the wire, it is not being set here.
-  #
-  def signout
-    flash[:error] = params[:error]
-
-    # def dont want this around (API tries to kill it, too)
-    cookies.delete(:_shelby_gt_common)
-
-    redirect_to Settings::ShelbyAPI.url + "/sign_out_user"
-  end
-
-  private
-
-
-    def get_dot_tv_roll_from_domain(request)
-      hard_coded_dot_tv_roll_id =
-        case request.host
-          #TODO: pull this mapping from API
-          when "bohrmann.tv" then "4f8f81bbb415cc4762002d7a"
-          when "chriskurdziel.tv" then "4f901d4bb415cc661405fde9"
-          when "danspinosa.tv" then "4f8f7ef2b415cc4762000002"
-          when "fredwilson.tv" then "4f8f7fb0b415cc4762000c42"
-          when "henrysztul.tv" then "4f8f7ef6b415cc476200004a"
-          when "hipstersounds.tv" then "4fa03429b415cc18bf0007b2"
-          when "laughingsquid.tv" then "4fc637879a725b755d001f77"
-          when "nerdfitness.tv" then "4fdee5f91c1cf4714200a607"
-          when "nowplaying.shelby.tv" then "4fcd0ca888ba6b07e30001d7"
-          when "reecepacheco.tv" then "4f900d56b415cc6614056681"
-          when "syria.shelby.tv" then "4fccffc188ba6b7a82000b92"
-          when "trololo.shelby.tv" then "4fccc6e4b415cc7f2100092d"
-          when "yvynyl.tv" then "4fa2908088ba6b61770010af"
-          when "nextlevelguy.tv" then "50d4f19ab415cc3807015105"    # requested via NF.tv, added 01/07/13 -hs
-          when "missouriquiltco.tv" then "51190f1eb415cc77f308eb22" # requested via Al Doan, added 02/10/13 -hs
-          when "mobilona.tv" then "511a6296b415cc12d0057dcf"        # requested by Julie, added 2/14/13 -ds
-          when "theadventurous.tv" then "511a62d4b415cc06c70a771b"  # requested by J.McFaddan, added 2/18/13 -hs
-          when "journeyful.tv" then "5114657bb415cc1ded5a399c" # requested by Moe I., added 3/7/13 -hs
-          when "chipsahoy.tv" then "513f4964b415cc143a00ec8b"
-          when "shannononeil.tv" then "51140d6db415cc1ded44830e" # requested by jamie@tierra-innovation.com, added 3/7/13 -hs
-          when "runneracademy.tv" then "508ed234b415cc50e40112bb" # requested by matt j., added 3/7/13 -hs
-          when "localhost.danspinosa.tv" then "4f8f7ef2b415cc4762000002"
-          when "localhost.henrysztul.tv" then "4f8f7ef6b415cc476200004a"
-          else nil
-        end
-
-      if hard_coded_dot_tv_roll_id
-        Shelby::API.get_roll(hard_coded_dot_tv_roll_id)
-      elsif [nil, "", "gt", "localhost", "www", "fb", "m"].include? request.subdomain
-        nil
-      elsif ActionDispatch::Http::URL.extract_domain(request.host) == "shelby.tv"
-        # for shelby.tv domain, try to find a roll assigned to the given subdomain
-        Shelby::API.get_roll_by_subdomain(request.subdomain)
-      else
-        nil
-      end
-    end
-
-    def get_genius_roll_id_from_path(path)
-      return @roll_id[1] if @roll_id = /fb\/genius\/roll\/(\w*)/i.match(path)
-    end
-
-    def get_frame_from_path(path)
-       frame_id = /(\w*)/.match(params[:path])
-       frame_id[1] if frame_id and BSON::ObjectId.legal?(frame_id[1])
-    end
-
-    def get_account_analytics_info(user)
-      if user
-        abilities = user["additional_abilities"]
-        abilities.keep_if {|a| a[0..2] == "UA-"}
-        return abilities.first if !abilities.empty?
-      end
-      return Settings::GoogleAnalytics.code
-    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,50 +1,7 @@
 ShelbyGtWeb::Application.routes.draw do
 
-  # Genius is broken and shouldn't be publicly available.
-  # get '/genius' => 'genius#index'
-  constraints(:subdomain => 'm') do
-    get '/' => 'mobile#search', :as => :mobile_search
-    get '/roll/:id' => 'mobile#roll', :as => :mobile_roll
-  end
-
-  #######################XXX###############################
-  # For development, take me out when mobile is more stable
-  if Rails.env.development?
-    get '/m' => 'mobile#search', :as => :mobile_search
-    get '/m/roll/:id' => 'mobile#roll', :as => :mobile_roll
-    constraints(:subdomain => 'm.localhost') do
-      get '/' => 'mobile#search', :as => :mobile_search # to show mobile search as shelby.tv ?
-      get '/roll/:id' => 'mobile#roll', :as => :mobile_roll
-    end
-  end
-  #######################XXX###############################
-
   # SEO PAGES
   get '/video/:provider_name/:provider_id(/*title)' => 'seovideo#show'
-
-  # FRAMES
-  get '/frame/:frame_id' => 'frame#just_frame'
-  get '/roll/:roll_id/frame/:frame_id' => 'frame#show'
-  get '/roll/:roll_id/frame/:frame_id/:frame_action' => 'frame#show'
-  get '/isolated-roll/:roll_id/frame/:frame_id' => 'frame#show_frame_in_isolated_roll'
-
-  # ROLLS
-  get '/roll/:roll_id/:title' => 'roll#show'
-  get '/roll/:roll_id' => 'roll#show'
-  get '/user/:user_id_or_nickname/personal_roll' => 'roll#show_personal_roll', :constraints => { :user_id_or_nickname => /[^\/]+/ }
-  get '/isolated-roll/:roll_id' => 'roll#show_isolated_roll'
-  get '/subscribe-via-email/roll/:roll_id' => 'roll#subscribe_via_email'
-
-  # DISCUSSION ROLLS
-  get '/mail/:roll_id' => 'discussion_roll#show'
-
-  # INVITES
-  get '/invite/:invite_id' => "signup#show"
-  get "invite" => 'signup#show', :as => :invite
-  post "invite" => 'signup#create'
-  get "signup" => 'signup#show', :as => :signup
-  get "signup/:code" => 'signup#show'
-  post "signup" => 'signup#create'
 
 
   # HOME
@@ -56,31 +13,12 @@ ShelbyGtWeb::Application.routes.draw do
   get '/search' => "home#search"
   get '/learn_more' => "home#learn_more"
 
-  get '/experience' => "home#experience"
-
-  get '/signout' => "home#signout", :as => :signout
-
-  #STATS
-  get '/user/:user_id/stats' => "home#stats"
-
-  #SHARES
-  get '/:user_id_or_nickname/shares(/:frame_id)' => "home#shares", :constraints => { :user_id_or_nickname => /[^\/]+/ }
-
-  # TURBO EMBED
-  get '/turbo_embellish' => 'turbo_embed#embellish'
-  get '/embed/:frame_id' => 'turbo_embed#embed'
-
   # used by vanity (A/B testing) to register partcipants via javascript
   match '/vanity/add_participant' => 'vanity#add_participant', :as => :add_participant
 
   # allow access to the vanity dashboard in development for manually selecting test alternatives
   match '/vanity(/:action(/:id(.:format)))', :controller=>:vanity if Rails.env.development?
 
-  # Everything else falls through to home#index
-  # This used to handle *everything* but now it's much more limited in scope
-  # XXX Still handles non-shelby-domain iso rolls :(
-  post '(*path)' => 'home#index', :as => :root
-  # first allow single segment paths that contain any character besides /, so we can handle things like /user.name
   get '(*path)' => 'home#index', :as => :root, :constraints => { :path => /[^\/]+/ }
   # otherwise catch any multi-segment path
   get '(*path)' => 'home#index', :as => :root
