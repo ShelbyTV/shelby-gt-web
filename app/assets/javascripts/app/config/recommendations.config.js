6600b7ae05c42b45e779c8911e2400f971223068
Michael Matyus <michael@maty.us>
2013-09-13 14:26:03 -0400

    recommendations UI improvement

--- app/assets/javascripts/app/config/recommendations.config.js
+++ app/assets/javascripts/app/config/recommendations.config.js
@@ -3,10 +3,24 @@ shelby.config.recommendations = {
     dashboardScanLimit : 20,
     minScore : 40.0
   },
-  avatars : {
-    entertainmentGraphRecommendation : '/images/recommendations/share-1.jpg',
-    videoGraphRecommendation         : '/images/recommendations/share-2.jpg',
-    mortarRecommendation             : '/images/recommendations/trending.jpg',
-    likeRecommendation               : '/images/recommendations/like.jpg'
+  entertainmentGraphRecommendation : {
+    avatar : '/images/recommendations/share-1.jpg',
+    color  : 'green',
+    header : 'Recommended for you'
+  },
+  videoGraphRecommendation : {
+    avatar : '/images/recommendations/share-2.jpg',
+    color  : 'green',
+    header : 'Recommended for you'
+  },
+  mortarRecommendation : {
+    avatar : '/images/recommendations/share-2.jpg',
+    color  : 'green',
+    header : 'Recommended for you'
+  },
+  likeRecommendation : {
+    avatar : '/images/recommendations/like.jpg',
+    color  : 'red',
+    header : 'Recommended for you'
   }
 };
