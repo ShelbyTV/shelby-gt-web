e7ed6ecc11c81a667b6fbf1a1d2a29eaf8cdc2ea
Michael Matyus <michael@maty.us>
2013-09-13 13:29:01 -0400

    progress on new recommendation UI

--- app/assets/javascripts/app/config/recommendations.config.js
+++ app/assets/javascripts/app/config/recommendations.config.js
@@ -2,5 +2,11 @@ shelby.config.recommendations = {
   videoGraph : {
     dashboardScanLimit : 20,
     minScore : 40.0
+  },
+  avatars : {
+    entertainmentGraphRecommendation : '/images/recommendations/share-1.jpg',
+    videoGraphRecommendation         : '/images/recommendations/share-2.jpg',
+    mortarRecommendation             : '/images/recommendations/trending.jpg',
+    likeRecommendation               : '/images/recommendations/like.jpg'
   }
 };
