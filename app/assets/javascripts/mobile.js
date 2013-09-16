0862fb653c829a745bb69aecb4aae450bfbde84f
Michael Matyus <michael@maty.us>
2013-09-16 02:27:23 -0400

    share panel; needs lots of refactoring to become abstract... or we should copy and paste the code so that it's not shared by the share pages

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,2 +1,8 @@
+//= require jquery
+
+//for sharing.
+//= require ./backbone/underscore.js
+//= require ../templates/share-page-form.jst.ejs
+
 //load event tracking js
 //= require ./event-tracking/mobile.tracking.js
