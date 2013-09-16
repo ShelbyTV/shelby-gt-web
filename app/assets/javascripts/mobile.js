cddaf6d6af970e4060323263edd271f85bccff0c
Michael Matyus <michael@maty.us>
2013-09-16 01:45:39 -0400

    @hsztul, remind me to remind you that we should probably pair a little on what's going in the mobile.js specific file

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,8 +1,2 @@
-//= require_tree ./common
-
 //load event tracking js
 //= require ./event-tracking/mobile.tracking.js
-
-
-//jQuery url params plugin
-//= require ./jquery-plugins/jquery.urlparams.js
