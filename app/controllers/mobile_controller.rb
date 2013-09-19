0efaeab44431aef19b3d00acd5766acff959888c
Henry <henry@sztul.com>
2013-09-19 14:00:09 -0400

    updating signup url and setting default service for onboarding

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -8,7 +8,8 @@ class MobileController < ApplicationController
     @is_mobile      = is_mobile?
 
     if user_signed_in? and @signed_in_user['app_progress'] and (@signed_in_user['app_progress']['onboarding'] != true)
-      authed_service = params[:service] || @signed_in_user['authentications'].first || "facebook"
+      users_first_auth = !@signed_in_user['authentications'].empty? ? @signed_in_user['authentications'].first
+      authed_service = params[:service] || users_first_auth['provider'] || "facebook"
       redirect_to mobile_show_onboarding_path(:step => 1, :service => authed_service)
     elsif user_signed_in?
       redirect_to mobile_stream_path
