d6df4cce1d200538b0816c2d146421b7cee59619
Henry <henry@sztul.com>
2013-09-23 11:18:38 -0400

    trying to get turbo straightened out

--- app/assets/javascripts/turbo.js
+++ app/assets/javascripts/turbo.js
@@ -1,5 +1,5 @@
 //This is loaded by our small embed snippet on a host page.
-//That snippet looks something like the contents of ./turbo-embed/turbo-bootstrap.html.erb
+//That snippet looks something like the contents of . / turbo-embed / turbo-bootstrap . html . erb
 
 // Turbo - Templates
 //= require_tree ../templates/turbo
