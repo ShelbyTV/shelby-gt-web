1bd1f14a6652a5c2808c424a81b205377b23dc1d
Michael Matyus <michael@maty.us>
2013-10-21 16:05:44 -0400

    app store sms pop up functioning

--- app/assets/javascripts/modules/twilio-appstore.js
+++ app/assets/javascripts/modules/twilio-appstore.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+  $('.js-sms-twilio').on('submit',function(e){
+    e.preventDefault();
+    $.ajax({
+      data: {
+        to: $(this).find('#sms')
+      },
+      type: 'POST',
+      url: e.target.action
+    });
+  });
 
-
+  $('.js-popup--sms').on('click',function(e){
+    if(this == e.target) { $(this).toggleClass('hidden',true); }
+    return false;
+  });
 });
