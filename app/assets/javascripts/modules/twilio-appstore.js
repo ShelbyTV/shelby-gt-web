249cb93f4d5dfe5af8024859f75b1238fc4dbc95
Michael Matyus <michael@maty.us>
2013-10-21 17:41:34 -0400

    return false; prevents bubbling, propagation, etc, etc

--- app/assets/javascripts/modules/twilio-appstore.js
+++ app/assets/javascripts/modules/twilio-appstore.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
   console.log('twilio-appstore');
-  $('#x').on('submit',function(e){
-    console.log('submit?');
-    // e.preventDefault();
-    // $.ajax({
-    //   data: {
-    //     to: $(this).find('#sms')
-    //   },
-    //   type: 'POST',
-    //   url: e.target.action
-    // });
+  $('.js-sms-twilio').on('submit',function(e){
+    e.preventDefault();
+    $.ajax({
+      data: {
+        to: $(this).find('#sms')
+      },
+      type: 'POST',
+      url: e.target.action
+    });
   });
 
   $('.js-popup--sms').on('click',function(e){
     if(this == e.target) { $(this).toggleClass('hidden',true); }
-    return false;
+    return;
   });
 });
