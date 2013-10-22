c4436866ea6a5416e85a5e794c712cc665d0ca03
Michael Matyus <michael@maty.us>
2013-10-22 15:52:27 -0400

    proper tracking'

--- app/assets/javascripts/modules/twilio-appstore.js
+++ app/assets/javascripts/modules/twilio-appstore.js
@@ -10,6 +10,13 @@ $(document).ready(function(){
 
     var $input = $this.find('#sms').attr('disabled',true);
 
+    var options = {
+      providers : ['ga'],
+      gaCategory : shelbyTrackingCategory,
+      gaAction : 'SMS Submission',
+      gaLabel : 'Success',
+    };
+
     $.ajax({
       data: {
         to: $input.val(),
@@ -22,17 +29,24 @@ $(document).ready(function(){
         var isSuccessful = (data.status === 200);
 
         $smsPopup.toggleClass('hidden',isSuccessful);
+
+        shelby.trackEx(options);
       },
       error: function(response){
         //TODO: Tell the user to check the number and try again
         $smsForm.find('.form_fieldset').toggleClass('form_fieldset--error',true);
         $input.removeAttr('disabled');
+
+        options.gaLabel = "Failed";
+        shelby.trackEx(options);
       }
     });
   });
 
   $smsPopup.on('click',function(e){
-    if(this == e.target) { $(this).toggleClass('hidden',true); }
+    if(this == e.target) {
+      $(this).toggleClass('hidden',true);
+    }
     return;
   });
 });
