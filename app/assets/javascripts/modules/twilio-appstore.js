ac82279dff135b49a000fe59979e148333411860
Michael Matyus <michael@maty.us>
2013-10-22 15:33:14 -0400

    more legit handling of sms form

--- app/assets/javascripts/modules/twilio-appstore.js
+++ app/assets/javascripts/modules/twilio-appstore.js
@@ -1,24 +1,37 @@
 $(document).ready(function(){
   console.log('twilio-appstore');
-  $('.js-sms-twilio').on('submit',function(e){
+  var $smsPopup = $('.js-popup--sms'),
+      $smsForm = $smsPopup.find('.js-sms-twilio');
+
+  $smsForm.on('submit',function(e){
     e.preventDefault();
+
+    $this = $(this);
+
+    var $input = $this.find('#sms').attr('disabled',true);
+
     $.ajax({
       data: {
-        to: $(this).find('#sms').val(),
+        to: $input.val(),
         type: 1
       },
       dataType: 'jsonp',
       url: e.target.action,
       success: function(data){
         //TODO: Display success
+        var isSuccessful = (data.status === 200);
+
+        $smsPopup.toggleClass('hidden',isSuccessful);
       },
       error: function(response){
         //TODO: Tell the user to check the number and try again
+        $smsForm.find('.form_fieldset').toggleClass('form_fieldset--error',true);
+        $input.removeAttr('disabled');
       }
     });
   });
 
-  $('.js-popup--sms').on('click',function(e){
+  $smsPopup.on('click',function(e){
     if(this == e.target) { $(this).toggleClass('hidden',true); }
     return;
   });
