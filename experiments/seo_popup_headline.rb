df2de8c306820d4709e4dc158cfdfe36809ef3e8
Henry <henry@sztul.com>
2013-10-24 16:26:54 -0400

    abc testing seo page copy. horray copy

---
+++ experiments/seo_popup_headline.rb
@@ -0,0 +1,5 @@
+ab_test "seo_popup_headline" do
+  description "Try different messaging on seo popup"
+  alternatives :discover_and_enjoy, :new_video_every_day, :want_more
+  metrics :tracked_externally
+end
