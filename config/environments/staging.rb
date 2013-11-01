19d7cbecf8ebcc8e6dbb200b3f9934f65ff2f3d3
Michael Matyus <michael@maty.us>
2013-11-01 15:21:30 -0400

    maybe this will get staging assets to work

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -47,7 +47,7 @@ ShelbyGtWeb::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  #config.action_controller.asset_host = "//staging.shelby.tv"
+  config.action_controller.asset_host = "//staging.shelby.tv"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile = %w(common.js deferred.js landing.js seovideo.js shares.js shares/shares_enabled.js shelby.js shelbify.js extension/includes.js mobile.js turbo.js experience.js signup.js png.css print.css screen.css landing.css seovideo.css extension.css experience.css mobile.css signup.css)
