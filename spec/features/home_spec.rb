6bcc730d1b3ab34c0050d967f5a24265ebe4c3a6
Michael Matyus <michael@maty.us>
2013-11-08 15:57:23 -0500

    login spec and home spec, respectively

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -7,43 +7,10 @@ describe 'Home Page', :type => :feature do
 
   context 'when initially logged out' do
 
-    it "loads the home page with all necessary elements" do
-      page.should have_field('username')
-      page.should have_field('password')
-      page.should have_button('Log In')
-      page.should have_button('Login')
-      page.should have_selector('.login-section')
-      page.should have_selector('.dropdown_module')
-    end
-
-    context 'after hovering over Log In', :js => true do
-
-      before(:each) do
-        find('.dropdown_module').hover
-      end
-
-      it "reveals the login options" do
-        find('.login-section').should be_visible
-      end
-
-      context 'when logging in with username and password' do
-
-        it "shows feedback for a failed login" do
-          fill_in('username', :with => 'badusername@#$')
-          click_button('Login')
-          page.should have_content('Please try again')
-        end
-
-        it "enters the app after a successful login" do
-          fill_in('username', :with => Settings::Acceptance.shelby_user_name)
-          fill_in('password', :with => Settings::Acceptance.shelby_password)
-          click_button('Login')
-          sleep(2)
-          page.should have_selector('#js-shelby-wrapper')
-        end
-
-      end
-
+    it "loads the home page with all the necessary elements" do
+      page.should have_selector('.shelby__head')
+      page.should have_selector('.shelby__wrapper')
+      page.should have_selector('.shelby__foot')
     end
 
   end
