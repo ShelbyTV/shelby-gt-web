72f74af1e3918ed2c4866f46620c552eefa3c4f4
matyus <michael@maty.us>
2013-11-13 16:50:45 -0500

    home page acceptance tests

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,53 +1,29 @@
 require 'spec_helper'
 
-describe 'Home Page', :type => :feature do
+describe 'Go to Home Page', :type => :feature do
 
-  context 'when device is a laptop or a desktop' do
-
-    before(:each) do
-      visit('/')
-    end
-
-    context 'when initially logged out' do
-
-      it 'loads the home page with all the necessary elements' do
-        page.should have_selector('.shelby__head')
-        page.should have_selector('.shelby__wrapper')
-        page.should have_selector('.shelby__foot')
-      end
-
-    end
-
+  before(:each) do
+    visit('/')
   end
 
-  context 'when device is a mobile phone' do
-
-    context 'on an iphone using safari' do
-
-      before(:each) do
-        page.driver.browser.header('User-Agent', Settings::Acceptance.ua_safari_mobile)
-        visit('/')
-      end
-
-      it 'loads the mobile layout for ios users' do
-        page.should have_selector('.shelby--mobile')
-      end
-
+  context 'select CTA', :js => true do
+    it 'loads the proper components' do
+      page.should have_selector('.js')
     end
 
-    context 'on an android mobile device' do
-
-      before(:each) do
-        page.driver.browser.header('User-Agent', Settings::Acceptance.ua_android_nexus)
-        visit('/')
+    it 'from an iOS device' do
+      page.driver.headers = { "User-Agent" => Settings::Acceptance.ua_safari_mobile }
+      within('#intro') do
+        click_link(Settings::Marketing.cta_button)
       end
-
-      it 'loads the mobile layout for non-ios users' do
-        page.should have_selector('.shelby--mobile')
-      end
-
     end
 
+    it 'from a non-iOS device' do
+      page.driver.headers = { "User-Agent" => Settings::Acceptance.ua_android_nexus }
+      within('#intro') do
+        click_link(Settings::Marketing.cta_button)
+        find('.js-popup--sms').should be_visible
+      end
+    end
   end
-
 end
