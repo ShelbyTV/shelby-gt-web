1092d62ea2685b64bdfee16a13a3b6752f8c0b42
Henry <henry@sztul.com>
2013-11-21 16:01:45 -0500

    trying to get out of the infinite loop

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -19,7 +19,7 @@ class MobileController < ApplicationController
       users_first_auth = !@signed_in_user['authentications'].empty? ? @signed_in_user['authentications'].first : {}
       authed_service = params[:service] || users_first_auth['provider'] || "facebook"
       redirect_to mobile_show_onboarding_path(:step => 1, :service => authed_service)
-    elsif user_signed_in? and (@signed_in_user['nickname'] != "Anonymous")
+    elsif user_signed_in?
       log_session()
       redirect_to mobile_stream_path
     else
