58149232c94d8d9f68d0e5d763287cc8e9dd386b
Henry <henry@sztul.com>
2013-11-21 16:08:43 -0500

    trying to get out of the infinite loop

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -32,7 +32,7 @@ class MobileController < ApplicationController
     if user_signed_in?
       @signed_in_user = check_for_signed_in_user
 
-      (redirect_to mobile_landing_path(:msg =>"You must be logged in.", :status => 401) and return) unless (@signed_in_user['nickname'] == 'Anonymous')
+      (redirect_to mobile_landing_path(:msg =>"You must be logged in.", :status => 401) and return) if (@signed_in_user['nickname'] == 'Anonymous')
       (redirect_to mobile_show_onboarding_path(:step => 1) and return) unless (@signed_in_user['app_progress'] and (@signed_in_user['app_progress']['onboarding'] == true))
 
       @is_mobile      = is_mobile?
