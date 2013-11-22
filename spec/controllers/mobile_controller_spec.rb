62159b749d53af98b10c9e7803f5d55ccb1ee96d
matyus <michael@maty.us>
2013-11-22 13:59:10 -0500

    new spec test file: controller test

---
+++ spec/controllers/mobile_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+# require 'json'
+
+describe MobileController do
+
+  #should_receive, should be called and should receive the "following arguments"
+  #unit vs. functional
+
+end
