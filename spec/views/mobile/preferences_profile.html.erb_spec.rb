d8adcebd2c848182bf7d78e24a8924216894be45
matyus <michael@maty.us>
2013-12-02 18:02:57 -0500

    render erros on profile properly

--- spec/views/mobile/preferences_profile.html.erb_spec.rb
+++ spec/views/mobile/preferences_profile.html.erb_spec.rb
@@ -24,5 +24,19 @@ describe "/m/preferences/profile" do
       rendered.should have_content user['name']
       rendered.should have_content user['nickname']
     end
+
+    it "renders an error for an occupied nickname" do
+      flash[:errors_nickname] = "has already been taken"
+
+      render :template => "mobile/preferences_profile", :layout => "layouts/mobile"
+      rendered.should have_content "Username has already been taken"
+    end
+
+    it "renders an error for an occupied nickname" do
+      flash[:errors_primary_email] = "has already been taken"
+
+      render :template => "mobile/preferences_profile", :layout => "layouts/mobile"
+      rendered.should have_content "Email has already been taken"
+    end
   end
 end
