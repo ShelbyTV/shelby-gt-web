d545f0abe16f8d89da93c5f1b48b0ef85e7c294b
matyus <michael@maty.us>
2014-01-13 11:00:39 -0500

    moving radar method into the bookmarklet route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -312,7 +312,38 @@ class HomeController < ApplicationController
 
   # THIS IS A TEMPORARY
   def bookmarklet
+    @found_video = params[:found_video] == "true" ? true : false
 
+    if @found_video
+
+      @signed_in_user = check_for_signed_in_user
+      # this means that the user isn't *really* logged in, delete the cookie and reassign variables appropriatly.
+      if @signed_in_user['app_progress'].nil?
+        cookies.delete(:_shelby_gt_common, :domain => '.shelby.tv')
+        @signed_in_user = check_for_signed_in_user
+      end
+
+      @user_signed_in = user_signed_in?
+      @videos = []
+
+      params.each do |provider_name, provider_ids|
+        # dont look at shit we dont support
+        break unless Settings::Radar.video_providers.include?(provider_name)
+
+        provider_ids = params[provider_name]
+        provider_ids.each do |provider_id|
+          video = Shelby::API.find_or_create_video(provider_name, provider_id)
+          # adding these because the find_or_create route doesn't add them :(
+          video['provider_name'] = provider_name
+          video['provider_id'] = provider_id
+          #####
+          @videos << video unless video.nil?
+        end
+      end
+      #use the "radar" layout/template if there are params the signify found videos
+      #otherwise, default layout is 'home/bookmarklet'
+      render :layout => 'radar', :template => 'radar/index'
+    end
   end
 
   private
