bdcdcfb0804cf39b897521b8d63f2536ad55d50f
matyus <michael@maty.us>
2014-02-04 15:17:46 -0500

    video asset

---
+++ app/assets/javascripts/shares/video.js
@@ -0,0 +1,45 @@
+var Video = Backbone.Model.extend({
+  composeKnownUrl : function () {
+    var knownUrl = "",
+        providerName = this.get('provider_name'),
+        videoId = this.get('id');
+
+    switch (providerName) {
+      case 'blip':
+        knownUrl = "http://blip.tv/episode/" + videoId;
+        break;
+      case 'youtube':
+        knownUrl = "http://www.youtube.com/watch?v=" + videoId;
+        break;
+      case 'dailymotion':
+        knownUrl = "http://www.dailymotion.com/video/" + videoId + "?";
+        break;
+      case 'vimeo':
+        knownUrl = "http://vimeo.com/" + videoId;
+        break;
+      case 'techcrunch':
+        knownUrl = "http://techcrunch.tv/watch?id=" + videoId;
+        break;
+      case 'collegehumor':
+        // knownUrl = (opts.useUrl) ? document.location.href : knownUrl = "http://collegehumor.com/video/" + videoId;
+        knownUrl = "http://collegehumor.com/video/" + videoId;
+        break;
+      case 'hulu':
+        //HACK (For now).
+        // the videoId is the content_id, not useful when hitting embedly or our link cache
+        // so using the page location for now
+        knownUrl = document.location.href;
+        break;
+      case 'bloomberg':
+        //HACK (For now).
+        // so using the page location + the provider id
+        knownUrl = document.location.href + "?bloomberg_ooyala_id="+ videoId;
+        break;
+      default:
+        knownUrl = false;
+    }
+
+    return knownUrl;
+  }
+
+});
