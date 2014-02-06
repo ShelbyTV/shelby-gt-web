956aea142336005765985ca1373121fb1a61d297
matyus <michael@maty.us>
2014-02-06 14:53:07 -0500

    moving sources to channels, erb and spec

---
+++ spec/views/mobile/preferences_channels.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "/m/preferences/channels" do
+
+  it "infers the controller path" do
+    controller.request.path_parameters[:controller].should eq("/m/preferences")
+  end
+
+  it "infers the action path" do
+    controller.request.path_parameters[:action].should eq("channels")
+  end
+
+  context "when page is visited" do
+    before(:each) do
+      view.stub(:csrf_token_from_cookie).and_return(true)
+      view.stub(:signed_in_user).and_return(true)
+      assign(:signed_in_user, user)
+    end
+
+    it "renders the channels" do
+      @quantity = 2
+      assign(:sources, sources(@quantity))
+      render :template => "mobile/preferences_channels", :layout => "layouts/mobile"
+      rendered.should have_selector('.user', :count => @quantity)
+    end
+  end
+end
