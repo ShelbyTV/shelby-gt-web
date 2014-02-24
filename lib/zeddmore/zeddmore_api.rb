f6923b39f80fa1372dc4e005513e2bbb2c8fbcdf
Henry <henry@sztul.com>
2014-02-24 13:56:46 -0500

    allowing sort_by trend rather than count

--- lib/zeddmore/zeddmore_api.rb
+++ lib/zeddmore/zeddmore_api.rb
@@ -5,8 +5,10 @@ module Zeddmore
     include HTTParty
     base_uri "#{Settings::Application.zeddmore_url}/v1"
 
-    def self.get_videos(date, interval)
-      r = get( "/videos/#{date}/#{interval}" ).parsed_response
+    def self.get_videos(date, interval, opts={})
+      route = "/videos/#{date}/#{interval}"
+      route += "?sort_by=#{opts[:sort_by]}" if opts[:sort_by]
+      r = get( route ).parsed_response
       if r['status'] == 'OK'
         return r['videos']
       else
