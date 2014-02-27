ac0166b9e787b61715c9d4f765c0dcb52076acfc
matyus <michael@maty.us>
2014-02-27 15:17:37 -0500

    nav component

---
+++ app/assets/javascripts/components/nav.js
@@ -0,0 +1,54 @@
+$(function(){
+  Shelby.Navbar = Backbone.View.extend({
+    options: {
+      sources : { bookmarklet: 'bookmarklet', shares: 'shares'}
+    },
+
+    el: $('.js-content-selector'),
+
+    events: {
+      'click .js-do-nothing'            : 'doNothing',
+      'click .js-stream'                : 'goToStream',
+      'click .js-explore'               : 'goToExplore',
+      'click .js-me'                    : 'goToMe',
+      'click .js-login-dropdown-button' : 'toggleDropdown',
+      'click .js-signout'               : 'goToSignout'
+    },
+
+    _navigate: function(path){
+      window.location = path;
+    },
+
+    doNothing: function(e){
+      e.preventDefault();
+    },
+
+    goToStream: function(e){
+      e.preventDefault();
+      this._navigate('/stream');
+    },
+
+    goToExplore: function(e){
+      e.preventDefault();
+      this._navigate('/explore');
+    },
+
+    goToMe: function(e){
+      e.preventDefault();
+      this._navigate('/'+Shelby.User.get('nickname'));
+    },
+
+    goToSignout: function(e){
+      e.preventDefault();
+      this._navigate('/signout');
+    },
+
+    toggleDropdown: function(e){
+      e.preventDefault();
+      var $dropdown = this.$el.find('.js-login-dropdown');
+
+      $dropdown.toggleClass('hidden',!$dropdown.hasClass('hidden'));
+    }
+
+  });
+});
