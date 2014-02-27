3df4b84f0a95cb3fa270330069a488d251f9abf3
matyus <michael@maty.us>
2014-02-27 16:21:19 -0500

    ugh

--- app/assets/javascripts/shares/shares_enabled.js
+++ app/assets/javascripts/shares/shares_enabled.js
@@ -1,14 +1,1 @@
-//REQUIRED
-//= require ../backbone/underscore.js
-//= require ../backbone/backbone.js
 
-//JST
-//= require ../../templates/share-page-form.jst.ejs
-
-//COMPONENTS
-//= require ./components/user.js
-//= require ./components/video.js
-//= require ./components/frame_interactions.js
-//= require ./components/guide.js
-
-var SharePanel = new GuideView();
