44a8f9fd75c7b1d4c8956db1e9f6271f09044a79
Henry <henry@sztul.com>
2014-03-05 09:28:54 -0500

    fixing display_banner bug && removing kiss metrics

--- app/assets/javascripts/event-tracking/landing.tracking.js
+++ app/assets/javascripts/event-tracking/landing.tracking.js
@@ -4,71 +4,70 @@
 // To Use:
 // add class 'js-track-event' to element with data attributes for a category, action and label
 //---------------------------------------------------------
-$(document).ready(function(){
+$(document).ready(function() {
 
-  shelbyTrackEventWithGlobalCategory = function(action, label){
-    var category = shelbyTrackingCategory;
-    action = action + " on " + category;
+    shelbyTrackEventWithGlobalCategory = function(action, label) {
+        var category = shelbyTrackingCategory;
+        action = action + " on " + category;
 
-    // Check if visitor is coming via a shl.by short link
-    //  NOTE: this is know because of the url param: ?awesm=shl.by_jk
-    var awesmLink = $.getUrlParam('awesm');
-    if (typeof awesmLink != 'undefined') {
-      category = category + ' via Short Link';
-      action = action + ' via Short Link';
-      label = awesmLink;
-    }
+        // Check if visitor is coming via a shl.by short link
+        //  NOTE: this is know because of the url param: ?awesm=shl.by_jk
+        var awesmLink = $.getUrlParam('awesm');
+        if (typeof awesmLink != 'undefined') {
+            category = category + ' via Short Link';
+            action = action + ' via Short Link';
+            label = awesmLink;
+        }
 
-    try{
-      _kmq.push(['record', action]);
-      _gaq.push(['_trackEvent', category, action, label]);
-    }
-    catch(e){
-      console.log("error posting event:", category, action, label, e);
+        try {
+            //_kmq.push(['record', action]);
+            _gaq.push(['_trackEvent', category, action, label]);
+        } catch (e) {
+            console.log("error posting event:", category, action, label, e);
+        };
     };
-  };
 
 
-  $('.js-track-event').on('touchstart click', function(e){
-    shelbyTrackEventWithGlobalCategory($(e.currentTarget).data("ga_action"),
-                                       $(e.currentTarget).data("ga_label"));
-  });
+    $('.js-track-event').on('touchstart click', function(e) {
+        shelbyTrackEventWithGlobalCategory($(e.currentTarget).data("ga_action"),
+            $(e.currentTarget).data("ga_label"));
+    });
 
-  $('form.js-track-submit').on('submit', function(e){
-    shelbyTrackEventWithGlobalCategory($(e.currentTarget).data("ga_action"),
-                                       $(e.currentTarget).data("ga_label"));
-  });
+    $('form.js-track-submit').on('submit', function(e) {
+        shelbyTrackEventWithGlobalCategory($(e.currentTarget).data("ga_action"),
+            $(e.currentTarget).data("ga_label"));
+    });
 });
 
 
 // FOR SPECIFIC EVENT TRACKING //
 if (typeof(shelby) == 'undefined') {
-  var shelby = {};
+    var shelby = {};
 }
 
-shelby.trackEx = function(options){
+shelby.trackEx = function(options) {
     // default options
     options = _.chain({}).extend(options).defaults({
-      providers : ['ga']
+        providers: ['ga']
     }).value();
 
     if (_(options.providers).contains('ga') && options.gaCategory) {
-      try {
-        if (_(options).has('gaValue')) {
-          _gaq.push(['_trackEvent', options.gaCategory, options.gaAction, options.gaLabel, options.gaValue]);
-        } else {
-          _gaq.push(['_trackEvent', options.gaCategory, options.gaAction, options.gaLabel]);
+        try {
+            if (_(options).has('gaValue')) {
+                _gaq.push(['_trackEvent', options.gaCategory, options.gaAction, options.gaLabel, options.gaValue]);
+            } else {
+                _gaq.push(['_trackEvent', options.gaCategory, options.gaAction, options.gaLabel]);
+            }
+        } catch (e) {
+            $.noop();
         }
-      } catch(e) {
-        $.noop();
-      }
     }
     var kmqName = options.kmqName || options.gaAction;
     if (_(options.providers).contains('kmq') && kmqName) {
-      try {
-        _kmq.push(['record', kmqName, _({}).extend(options.kmqProperties)]);
-      } catch(e) {
-        $.noop();
-      }
+        try {
+            //_kmq.push(['record', kmqName, _({}).extend(options.kmqProperties)]);
+        } catch (e) {
+            $.noop();
+        }
     }
-  };
+};
