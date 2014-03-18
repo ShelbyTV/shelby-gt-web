43f9f5c4d456b2578ba6566e7606c837f7e1be36
matyus <michael@maty.us>
2014-03-18 14:47:49 -0400

    @hsztul I added in some automated tests that ensure shelves have the proper copy, button URLs, forms, login UI for the landing page

--- spec/views/home/landing.html.erb_spec.rb
+++ spec/views/home/landing.html.erb_spec.rb
@@ -23,7 +23,7 @@ describe "home/landing" do
     end
   end
 
-  context "main call to action" do
+  context "main call to action shelf" do
     describe "home/shelf/call_to_action" do
       context "renders the main 'call to action' for amazon kindle" do
         before(:all) do
@@ -47,12 +47,67 @@ describe "home/landing" do
           render
           #CTA should point to the App Store
           #and use the Marketing button copy
-          rendered.should =~ /#{Settings::Marketing.cta_button}/
           rendered.should =~ /#{Settings::Application.ios_app_url}/
+          rendered.should =~ /#{Settings::Marketing.cta}/
+        end
+
+        it "renders the proper copy from marketing.yml" do
+          render
+          rendered.should =~ /#{Settings::Marketing.cta_body}/
+          rendered.should =~ /#{Settings::Marketing.cta_button}/
         end
       end
     end
   end
+
+  context "social networks shelf" do
+    describe "home/shelf/social" do
+      context "renders the secondary 'call to action' for amazon kindle" do
+        before(:all) do
+          assign(:mobile_os,:amazon)
+        end
+
+        it "renders the CTA with the amazon-specific url" do
+          render
+          #amazon view uses a form with the route /user/create
+          rendered.should have_selector('.js-get-started-form')
+          rendered.should =~ /user\/create/
+        end
+      end
+
+      context "renders the main 'call to action' for web and mobile web" do
+        before(:all) do
+          assign(:mobile_os,nil)
+        end
+
+        it "renders the CTA with the App Store url" do
+          render
+          rendered.should =~ /#{Settings::Application.ios_app_url}/
+          rendered.should =~ /#{Settings::Marketing.cta_button}/
+        end
+
+        it "renders the proper copy from marketing.yml" do
+          render
+          #use marketing.yml for copy
+          rendered.should =~ /#{Settings::Marketing.social}/
+          rendered.should =~ /#{Settings::Marketing.social_body}/
+        end
+      end
+    end
+  end
+
+  context "stream shelf" do
+    describe "home/shelf/social" do
+      context "renders the main 'call to action' for web and mobile web" do
+        it "renders the proper copy from marketing.yml" do
+          render
+          rendered.should =~ /#{Settings::Marketing.stream}/
+          rendered.should =~ /#{Settings::Marketing.stream_body}/
+        end
+      end
+    end
+  end
+
 
 end
 
