5c8f2d6c10a9bda45cc9749b1768817707fd2390
matyus <michael@maty.us>
2014-03-18 14:36:04 -0400

    commenting out tests for twilio feature that doesn't exist anymore

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -11,36 +11,36 @@ describe 'Go to Home Page', :type => :feature do
       page.should have_selector('.js')
     end
 
-    it 'from an iOS device' do
-      page.driver.headers = { "User-Agent" => Settings::Acceptance.ua_safari_mobile }
-      find('#js-cta--main').click
-    end
+    # it 'from an iOS device' do
+    #   page.driver.headers = { "User-Agent" => Settings::Acceptance.ua_safari_mobile }
+    #   find('#js-cta--main').click
+    # end
 
-    it 'from a non-iOS device' do
-      page.driver.headers = { "User-Agent" => Settings::Acceptance.ua_android_nexus }
-      find('#js-cta--main').click
-      page.should have_content('Text iOS download link to your phone:')
-    end
+    # it 'from a non-iOS device' do
+    #   page.driver.headers = { "User-Agent" => Settings::Acceptance.ua_android_nexus }
+    #   find('#js-cta--main').click
+    #   page.should have_content(Settings::Marketing.cta_button)
+    # end
 
-    context 'and fills out the SMS form' do
-      before(:each) do
-        find('#js-cta--main').click
-      end
+    # context 'and fills out the SMS form' do
+    #   before(:each) do
+    #     find('#js-cta--main').click
+    #   end
 
-      it 'successfully' do
-        fill_in('sms', :with => '9178285740')
-        click_on('Send')
-        sleep(5)
-        find('.js-sms-success').should be_visible
-      end
+    #   it 'successfully' do
+    #     fill_in('sms', :with => '9178285740')
+    #     click_on('Send')
+    #     sleep(5)
+    #     find('.js-sms-success').should be_visible
+    #   end
 
-      it 'unsuccessfully' do
-        fill_in('sms', :with => 'invalidphonenumber')
-        click_on('Send')
-        sleep(5)
-        page.has_css?('.form_fieldset--error')
-      end
-    end
+    #   it 'unsuccessfully' do
+    #     fill_in('sms', :with => 'invalidphonenumber')
+    #     click_on('Send')
+    #     sleep(5)
+    #     page.has_css?('.form_fieldset--error')
+    #   end
+    # end
 
   end
 end
