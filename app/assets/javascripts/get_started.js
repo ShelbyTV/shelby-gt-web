9c2d6ec307bc9b0c2cd34cfb369580e4628fa77d
matyus <michael@maty.us>
2014-03-24 15:36:18 -0400

    adding loading spinner fanciness to get started button'

--- app/assets/javascripts/get_started.js
+++ app/assets/javascripts/get_started.js
@@ -1,4 +1,15 @@
 //= require jquery
 $(function(){
   $('.js-shelf').height(window.innerHeight);
+
+  $('.js-get-started-button').on('click', function(e){
+    e.preventDefault();
+    var $target = $(e.currentTarget);
+    if (!$target.hasClass('button_busy')) {
+      $target.addClass('button_busy');
+      window.setTimeout(function(){
+        $(e.currentTarget.form).submit();
+      }, 500);
+    }
+  });
 });
