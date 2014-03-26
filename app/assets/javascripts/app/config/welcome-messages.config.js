447802ed8820568b1ec4722ef9a8a38df41257db
Joshua Samberg <josh@shelby.tv>
2014-03-26 16:00:42 -0400

    Show welcome education message first time anonymous user visits stream

--- app/assets/javascripts/app/config/welcome-messages.config.js
+++ app/assets/javascripts/app/config/welcome-messages.config.js
@@ -1,22 +1,7 @@
 shelby.config.welcomeMessages = {
   dashboard: {
-    header: "Welcome to Shelby.tv",
-    message: "This stream of video was made just for you. The more you watch, like and share the better it will get!",
-    button: "Play Your Stream"
-  },
-  channel: {
-    header: "Explore Shelby.tv",
-    message: "This stream is what we think of as the best video on Shelby right now. Watch, discover, enjoy!",
-    button: "Play video!"
-  },
-  ownShares: {
-    header: "Welcome to your Profile",
-    message: "Here you can find the videos you've shared and liked on Shelby.tv",
+    header: "Welcome to Shelby.tv!",
+    message: "Follow some channels or connect Facebook to fill your stream with videos tailored to you.",
     button: "Got it!"
   },
-  loggedOutShares : {
-    header: "Welcome to Shelby.tv",
-    message : " uses Shelby to watch, share and discover amazing video.",
-    button: "Play"
-  }
 };
