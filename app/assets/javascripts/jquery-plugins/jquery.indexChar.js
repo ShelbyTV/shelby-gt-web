f66bf4e2fcd9da4cb35ae7db66bba6e19adfd213
matyus <michael@maty.us>
2014-04-02 16:51:56 -0400

    update to awesome jquery plugin

--- app/assets/javascripts/jquery-plugins/jquery.indexChar.js
+++ app/assets/javascripts/jquery-plugins/jquery.indexChar.js
@@ -1,26 +1,2 @@
-/**
- * @license IndexChar, get the index of the given character in the specified alphabet.
- * *
- * neat little indexing trick inspiration: http://stackoverflow.com/a/9906256
- *
- * Copyright 2014, Michael Matyus
- * Version: 0
- */
-(function($){
-  $.charIndex = function(str,opts){
-  //if default setting is a number, it must be the Index Character
-  if(typeof(opts) === 'number') {
-    opts = {indexChar:opts};
-  }
-
-  var settings = $.extend({}, $.charIndex.defaults, opts),
-      glyph    = str.charAt(settings.indexChar).toUpperCase();
-
-    return settings.scale.indexOf(glyph);
-  };
-
-  $.charIndex.defaults = {
-    indexChar: 0,
-    scale    :'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  };
-}(jQuery));
+/*! jQuery.indexChar 2014-04-02 */
+!function(a){a.indexChar=function(b,c){if("number"==typeof b)return void 0;if("number"==typeof c){if(b.length<=c)return void 0;c={indexChar:c}}var d=a.extend({},a.indexChar.defaults,c),e=b.charAt(d.indexChar).toUpperCase();return d.scale=d.scale.toUpperCase(),d.scale.indexOf(e)},a.indexChar.defaults={indexChar:0,scale:"ABCDEFGHIJKLMNOPQRSTUVWXYZ"}}(jQuery);
