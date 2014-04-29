a083dd80d7ed29feabefd86f0f575ffc2cf1cb0e
matyus <michael@maty.us>
2014-04-29 16:48:57 -0400

    simplifying

--- app/assets/javascripts/cheez.js
+++ app/assets/javascripts/cheez.js
@@ -1,32 +1,9 @@
-//= require ./Cheez/slim_v05.js
-
-//= require ./Cheez/client.js
-//= require ./Cheez/all.js
-//= require ./Cheez/platform.js
-//= require ./Cheez/widgets.js
-//= require ./Cheez/all(1).js
-//= require ./Cheez/p.js
-//= require ./Cheez/quant.js
-//= require ./Cheez/beacon.js
-//= require ./Cheez/modernizr.min.js
-
 //= require ./Cheez/jquery.js
 //= require ./Cheez/underscore-1.5.1.js
 //= require ./Cheez/backbone-1.1.0.js
 //= require ./Cheez/backbone-relational-0.8.7.js
-//= require ./Cheez/search.js
 
-//= require ./Cheez/SearchRouter.js
-//= require ./Cheez/SearchModel.js
-//= require ./Cheez/SearchPageView.js
-//= require ./Cheez/SearchResultCardCollectionView.js
-//= require ./Cheez/SearchResultCardView.js
-//= require ./Cheez/SearchResultFilterControlsView.js
-
-//= require ./Cheez/gpt.js
-
-//= require ./Cheez/main-new-search.js
-
+//= require ./Cheez/require.js
 
 $(document).ready(function(){
 
